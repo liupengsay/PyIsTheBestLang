4292cd5fbb96395420336ec8b8f6f4558812772d
ThinkPad <liupengsay@foxmail.com>
2022-03-17 21:23:14 +0800

    字节一面面试coding题目

--- leetcode/solution.py
+++ leetcode/solution.py
@@ -1,5 +1,29 @@
 
 class Solution:
+    @staticmethod
+    def num_str(s):
+        stack = []
+        for i, va in enumerate(s):
+            if va != ']':
+                stack.append(va)
+            elif va == ']':
+                st = ''
+                while stack:
+                    if stack[-1] != '[':
+                        st = stack.pop(-1) + st
+                    else:
+                        break
+                stack.pop(-1)
+                num = ''
+                while stack:
+                    if stack[-1].isnumeric():
+                        num = stack.pop(-1) + num
+                    else:
+                        break
+                if num == '':
+                    num = 1
+                stack.extend(list(int(num)*st))
+        return ''.join(stack)
 
     @staticmethod
     def two_sum(nums, target):
--- leetcode/tests/test_solution.py
+++ leetcode/tests/test_solution.py
@@ -1,10 +1,21 @@
 import logging
 import unittest
+from leetcode.solution import Solution
 
 
 class Test(unittest.TestCase):
-    def test_function_1(self):
-        logging.info('begin')
+    def test_num_str(self):
+        s = "3[b]2[ac]"
+        self.assertEqual("bbbacac", Solution.num_str(s))
+
+        s = "3[b2[ac]]"
+        self.assertEqual("bacacbacacbacac", Solution.num_str(s))
+
+        s = "3[b]2[ac]ef"
+        self.assertEqual("bbbacacef", Solution.num_str(s))
+
+        s = "ef3[b]2[ac]"
+        self.assertEqual("efbbbacac", Solution.num_str(s))
         return
 
 
