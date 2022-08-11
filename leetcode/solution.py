cd9e132b4accad2755dcf8463d7410e8c454018b
ThinkPad <liupengsay@foxmail.com>
2022-08-12 07:52:59 +0800

    备份更新

--- leetcode/solution.py
+++ leetcode/solution.py
@@ -1,56 +1,51 @@
+import itertools
 from typing import List
 
 
-import math
+class Trie:
+    def __init__(self):
+        self.dct = dict()
+        return
+
+    def update(self, word):
+        cur = self.dct
+        for w in word:
+            if w not in cur:
+                cur[w] = dict()
+            cur = cur[w]
+        return
 
 
-def euler_phi(n):
-    ans = n
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            ans = ans // i * (i - 1)
-            while n % i == 0:
-                n = n / i
-    if n > 1:
-        ans = ans // n * (n - 1)
-    return int(ans)
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        target = [0]*26
+        for w in p:
+            target[ord(w)-ord('a')] += 1
 
-def Eratosthenes(n):
-    p = 0
-    is_prime = [True]*(n+1)
-    is_prime[0] = is_prime[1] = False
-    for i in range(2, n + 1):
-        if is_prime[i]:
-            is_prime[p] = i
-            p = p + 1
-            if i * i <= n:
-                j = i * i
-                while j <= n:
-                    is_prime[j] = False
-                    j = j + i
-    return p
+        n = len(p)
+        ans = []
+        cur = [0]*26
+        for i, w in enumerate(s):
+            cur[ord(w)-ord('a')] += 1
+            if i >= n:
+                cur[ord(s[i-n])-ord('a')] += 1
+                if cur == target:
+                    ans.append(i)
+        return ans
 
-def sieve_of_eratosthenes(n):#埃拉托色尼筛选法，返回少于n的素数
-    primes = [True] * (n+1)#范围0到n的列表
-    p = 2#这是最小的素数
-    while p * p <= n:#一直筛到sqrt(n)就行了
-        if primes[p]:#如果没被筛，一定是素数
-            for i in range(p * 2, n + 1, p):#筛掉它的倍数即可
-                primes[i] = False
-        p += 1
-    primes = [element for element in range(2, n+1) if primes[element]]#得到所有少于n的素数
-    return len(primes)
 
-print(euler_phi(108))
-print(euler_phi(1))
-print(euler_phi(2))
-print(euler_phi(10))
-print(Eratosthenes(108))
-print(Eratosthenes(1))
-print(Eratosthenes(2))
-print(Eratosthenes(10))
 
-print(sieve_of_eratosthenes(108))
-print(sieve_of_eratosthenes(1))
-print(sieve_of_eratosthenes(2))
-print(sieve_of_eratosthenes(10))
+
+
+
+
+
+
+
+
+
+
+[73,55,36,5,55,14,9,7,72,52]
+32
+69
+[-1, 1, 1, 0, 1, 0, 0, 0, -1, 1]
