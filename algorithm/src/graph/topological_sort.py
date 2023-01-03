98da2acb7c01335e18bee59c50cf7baf4d27c75d
liupengsay <liupengsay@foxmail.com>
2023-01-03 23:24:03 +0800

    内向基环树（有向与无向）拓扑排序

--- algorithm/src/graph/topological_sort.py
+++ algorithm/src/graph/topological_sort.py
@@ -3,13 +3,16 @@
 """
 
 """
-算法：拓扑排序
+算法：拓扑排序、内向基环树（有向或者无向，连通块有k个节点以及k条边）
 功能：有向图进行排序，无向图在选定根节点的情况下也可以进行拓扑排序
 题目：xx（xx）
 
 L2392 给定条件下构造矩阵（https://leetcode.cn/problems/build-a-matrix-with-conditions/）分别通过行列的拓扑排序来确定数字所在索引
 L2127 参加会议的最多员工数（https://leetcode.cn/problems/maximum-employees-to-be-invited-to-a-meeting/）拓扑排序确定内向基环，按照环的大小进行贪心枚举
-
+（无向图的内向基环树，求简单路径的树枝连通）F - Well-defined Path Queries on a Namori：https://atcoder.jp/contests/abc266/submissions/me
+内向基环树介绍：https://leetcode.cn/problems/maximum-employees-to-be-invited-to-a-meeting/solution/nei-xiang-ji-huan-shu-tuo-bu-pai-xu-fen-c1i1b/
+2360. 图中的最长环（https://leetcode.cn/problems/longest-cycle-in-a-graph/）
+2127. 参加会议的最多员工数（https://leetcode.cn/problems/maximum-employees-to-be-invited-to-a-meeting/）
 
 参考：OI WiKi（xx）
 """
@@ -68,6 +71,43 @@ class TopologicalSort:
             stack = nex
             step += 1
         return visit
+
+    # 内向基环树写法 https://atcoder.jp/contests/abc266/submissions/37717739
+    # def main(ac=FastIO()):
+    #     n = ac.read_int()
+    #     edge = [[] for _ in range(n)]
+    #     uf = UnionFind(n)
+    #
+    #     degree = [0] * n
+    #     for _ in range(n):
+    #         u, v = ac.read_ints_minus_one()
+    #         edge[u].append(v)
+    #         edge[v].append(u)
+    #         degree[u] += 1
+    #         degree[v] += 1
+    #
+    #     que = deque()
+    #     for i in range(n):
+    #         if degree[i] == 1:
+    #             que.append(i)
+    #     while que:
+    #         now = que.popleft()
+    #         nex = edge[now][0]
+    #         degree[now] -= 1
+    #         degree[nex] -= 1
+    #         edge[nex].remove(now)
+    #         uf.union(now, nex)
+    #         if degree[nex] == 1:
+    #             que.append(nex)
+    #
+    #     q = ac.read_int()
+    #     for _ in range(q):
+    #         x, y = ac.read_ints_minus_one()
+    #         if uf.is_connected(x, y):
+    #             ac.st("Yes")
+    #         else:
+    #             ac.st("No")
+    #     return
 
 
 class TestGeneral(unittest.TestCase):
