6b2f19eee368bfe70c5642ef3f3e1335f60cd6e1
liupengsay <liupengsay@foxmail.com>
2023-01-04 23:20:06 +0800

    中缀表达式支持负数

--- algorithm/src/string/expression.py
+++ algorithm/src/string/expression.py
@@ -1,3 +1,7 @@
+
+import unittest
+
+
 """
 
 """
@@ -10,33 +14,6 @@ P1175 表达式的转换（https://www.luogu.com.cn/problem/P1175）
 
 参考：OI WiKi（xx）
 """
-
-import bisect
-import random
-import re
-import unittest
-
-from typing import List
-import heapq
-import math
-from collections import defaultdict, Counter, deque
-from functools import lru_cache
-from itertools import combinations
-from sortedcontainers import SortedList, SortedDict, SortedSet
-
-from sortedcontainers import SortedDict
-from functools import reduce
-from operator import xor
-from functools import lru_cache
-
-import random
-from itertools import permutations, combinations
-import numpy as np
-
-from decimal import Decimal
-
-import heapq
-import copy
 
 
 class Node(object):
@@ -52,30 +29,50 @@ class TreeExpression:
 
     def exp_tree(self, s: str) -> 'Node':
         # 只剩数字的情况
-        if s.isnumeric():
+        try:
+            int(s)
             return Node(s)
+        except ValueError as _:
+            pass
 
-        # 不支持 2*-3 和 -2+3 的情形即要求所有数字为非负数
+        # 处理-(的情形
+        if len(s) >= 2 and s[0] == "-" and s[1] == "(":
+            cnt = 0
+            for i, w in enumerate(s):
+                if w == "(":
+                    cnt += 1
+                elif w == ")":
+                    cnt -= 1
+                    if not cnt:
+                        pre = s[1:i].replace("+", "-").replace("-", "+")
+                        s = pre + s[i:]
+                        break
+
+        # 处理包含负数的情形
+        neg = ""
+        if s[0] == "-":
+            neg = "-"
+            s = s[1:]
+
         n = len(s)
         cnt = 0
-
         # 按照运算符号的优先级倒序遍历字符串
         for i in range(n - 1, -1, -1):
             cnt += int(s[i] == ')') - int(s[i] == '(')
             if s[i] in ['+', '-'] and not cnt:
-                return Node(s[i], self.exp_tree(s[:i]), self.exp_tree(s[i + 1:]))
+                return Node(s[i], self.exp_tree(neg + s[:i]), self.exp_tree(s[i + 1:]))
 
         # 注意是从后往前
         for i in range(n - 1, -1, -1):
             cnt += int(s[i] == ')') - int(s[i] == '(')
             if s[i] in ['*', '/'] and not cnt:
-                return Node(s[i], self.exp_tree(s[:i]), self.exp_tree(s[i + 1:]))
+                return Node(s[i], self.exp_tree(neg + s[:i]), self.exp_tree(s[i + 1:]))
 
         # 注意是从前往后
         for i in range(n):
             cnt += int(s[i] == ')') - int(s[i] == '(')
             if s[i] in ['^'] and not cnt:  # 这里的 ^ 表示幂
-                return Node(s[i], self.exp_tree(s[:i]), self.exp_tree(s[i + 1:]))
+                return Node(s[i], self.exp_tree(neg + s[:i]), self.exp_tree(s[i + 1:]))
 
         # 其余则是开头结尾为括号的情况
         return self.exp_tree(s[1:-1])
@@ -114,14 +111,15 @@ class TreeExpression:
             pre = stack[:]
         ans.append(pre)
         return ans
-    
-    
+
+
 class TestGeneral(unittest.TestCase):
 
     def test_tree_expression(self):
         te = TreeExpression()
-        s = "2*3^4^2+(5/2-2)"
-        assert int(te.main_1175(s)[-1][0]) == eval(s.replace("^", "**").replace("/", "//"))
+        lst = ["2*3^4^2+(5/2-2)", "-2+3", "2*(-5/2+3*2)-33", "((-2+3)*3+5-7/2)^2", "2*(-3)", "0-(-3)", "-(-3)+2"]
+        for s in lst:
+            assert int(te.main_1175(s)[-1][0]) == eval(s.replace("^", "**").replace("/", "//"))
         return
 
 
