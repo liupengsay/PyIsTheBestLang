e3a39d9008670a00d155ba55f647a6d1cc85bcd0
liupengsay <liupengsay@foxmail.com>
2023-01-09 08:31:45 +0800

    滑动指针窗口栈加队列

--- algorithm/tests/problem_1.py
+++ algorithm/tests/problem_1.py
@@ -35,68 +35,19 @@ def mmin(a, b):
     return a if a < b else b
 
 
-# 标准并查集
-class UnionFind:
-    def __init__(self, n):
-        self.root = [i for i in range(n)]
-        self.size = [1] * n
-        self.part = n
-
-    def find(self, x):
-        if x != self.root[x]:
-            # 在查询的时候合并到顺带直接根节点
-            root_x = self.find(self.root[x])
-            self.root[x] = root_x
-            return root_x
-        return x
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if root_x == root_y:
-            return False
-        if self.size[root_x] >= self.size[root_y]:
-            root_x, root_y = root_y, root_x
-        self.root[root_x] = root_y
-        self.size[root_y] += self.size[root_x]
-        # 将非根节点的秩赋0
-        self.size[root_x] = 0
-        self.part -= 1
-        return True
-
-    def is_connected(self, x, y):
-        return self.find(x) == self.find(y)
-
-    def get_root_part(self):
-        # 获取每个根节点对应的组
-        part = defaultdict(list)
-        n = len(self.root)
-        for i in range(n):
-            part[self.find(i)].append(i)
-        return part
-
-    def get_root_size(self):
-        # 获取每个根节点对应的组大小
-        size = defaultdict(int)
-        n = len(self.root)
-        for i in range(n):
-            size[self.find(i)] = self.size[self.find(i)]
-        return size
-
 
 
 class Solution:
-    def maximumCount(self, nums: List[int]) -> int:
-        pos = neg = 0
-        for num in nums:
-            if num > 0:
-                pos += 1
-            if num < 0:
-                pos += 1
-        return max(pos, neg)
-
-
-
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")]*(amount+1)
+        dp[0] = 1
+        for num in coins:
+            for i in range(num, amount+1):
+                if dp[i-num] + 1 < dp[i]:
+                    dp[i] = dp[i-num] + 1
+        if dp[-1] < float("inf"):
+            return dp[-1]
+        return -1
 
 class TestGeneral(unittest.TestCase):
     def test_solution(self):
