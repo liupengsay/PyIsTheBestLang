2cd78d9f3f855cbfc1d06f1d7a90d1d1d61907f8
liupengsay <liupengsay@foxmail.com>
2023-01-12 07:30:43 +0800

    增加题目

--- algorithm/src/dp/matrix_dp.py
+++ algorithm/src/dp/matrix_dp.py
@@ -1,6 +1,7 @@
-"""
+import unittest
 
-"""
+from typing import List
+
 """
 算法：矩阵DP
 功能：在二维矩阵上进行转移的DP，经典的有矩阵前缀和，矩阵区间和，正方形最大边长或面积
@@ -9,40 +10,39 @@ L2435 矩阵中和能被 K 整除的路径（https://leetcode.cn/problems/paths-
 L2088 统计农场中肥沃金字塔的数目（https://leetcode.cn/problems/count-fertile-pyramids-in-a-land/）类似求正方形的边长和面积进行矩阵DP
 P1681 最大正方形II（https://www.luogu.com.cn/problem/P1681）求黑白格子相间的最大正方形面积
 221. 最大正方形（https://leetcode.cn/problems/maximal-square/）求全为 1 的最大正方形面积
+
+P2049 魔术棋子（https://www.luogu.com.cn/problem/P2049）求左上角到右下角所有路径的乘积取模数
 参考：OI WiKi（xx）
 """
-
-import bisect
-import random
-import re
-import unittest
-
-from typing import List
-import heapq
-import math
-from collections import defaultdict, Counter, deque
-from functools import lru_cache
-from itertools import combinations
-from sortedcontainers import SortedList, SortedDict, SortedSet
-
-from sortedcontainers import SortedDict
-from functools import reduce
-from operator import xor
-from functools import lru_cache
-
-import random
-from itertools import permutations, combinations
-import numpy as np
-
-from decimal import Decimal
-
-import heapq
-import copy
 
 
 class MatrixDP:
     def __init__(self):
         return
+
+    @staticmethod
+    def path_mul_mod(m, n, k, grid):
+        # 求矩阵左上角到右下角的乘积取模数
+        dp = [[set() for _ in range(n)] for _ in range(m)]
+        dp[0][0].add(grid[0][0] % k)
+        for i in range(1, m):
+            x = grid[i][0]
+            for p in dp[i - 1][0]:
+                dp[i][0].add((p * x) % k)
+        for j in range(1, n):
+            x = grid[0][j]
+            for p in dp[0][j - 1]:
+                dp[0][j].add((p * x) % k)
+
+        for i in range(1, m):
+            for j in range(1, n):
+                x = grid[i][j]
+                for p in dp[i][j - 1]:
+                    dp[i][j].add((p * x) % k)
+                for p in dp[i - 1][j]:
+                    dp[i][j].add((p * x) % k)
+        ans = sorted(list(dp[-1][-1]))
+        return ans
 
     @staticmethod
     def maximal_square(matrix: List[List[str]]) -> int:
@@ -55,7 +55,8 @@ class MatrixDP:
             for j in range(n):
                 if matrix[i][j] == "1":
                     # 转移公式
-                    dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1
+                    dp[i + 1][j + 1] = min(dp[i][j],
+                                           dp[i + 1][j], dp[i][j + 1]) + 1
                     if dp[i + 1][j + 1] > ans:
                         ans = dp[i + 1][j + 1]
         return ans ** 2
@@ -65,7 +66,8 @@ class TestGeneral(unittest.TestCase):
 
     def test_matrix_dp(self):
         md = MatrixDP()
-        matrix = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
+        matrix = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], [
+            "1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
         assert md.maximal_square(matrix) == 4
         return
 
