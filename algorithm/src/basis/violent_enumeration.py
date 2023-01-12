fe41d1de9f99d5549b7a2e2e70c563d673754b99
liupengsay <liupengsay@foxmail.com>
2023-01-12 08:11:21 +0800

    矩阵旋转

--- algorithm/src/basis/violent_enumeration.py
+++ algorithm/src/basis/violent_enumeration.py
@@ -23,13 +23,11 @@ P2128 赤壁之战（https://www.luogu.com.cn/problem/P2128）枚举完全图的
 参考：OI WiKi（xx）
 """
 
-
-
-
 import bisect
 import random
 import re
 import unittest
+
 from typing import List
 import heapq
 import math
@@ -37,16 +35,22 @@ from collections import defaultdict, Counter, deque
 from functools import lru_cache
 from itertools import combinations
 from sortedcontainers import SortedList, SortedDict, SortedSet
+
 from sortedcontainers import SortedDict
 from functools import reduce
 from operator import xor
 from functools import lru_cache
+
 import random
 from itertools import permutations, combinations
 import numpy as np
+
 from decimal import Decimal
+
 import heapq
 import copy
+
+
 class ViolentEnumeration:
     def __init__(self):
         return
@@ -58,45 +62,15 @@ class ViolentEnumeration:
         n = len(matrix)
         for i in range(n // 2):
             for j in range((n + 1) // 2):
-                matrix[i][j], matrix[n -
-                                     j -
-                                     1][i], matrix[n -
-                                                   i -
-                                                   1][n -
-                                                      j -
-                                                      1], matrix[j][n -
-                                                                    i -
-                                                                    1] = matrix[n -
-                                                                                j -
-                                                                                1][i], matrix[n -
-                                                                                              i -
-                                                                                              1][n -
-                                                                                                 j -
-                                                                                                 1], matrix[j][n -
-                                                                                                               i -
-                                                                                                               1], matrix[i][j]
+                a, b, c, d = matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1], matrix[i][j]
+                matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1] = a, b, c, d
 
         # 将矩阵逆时针旋转 90 度
         n = len(matrix)
         for i in range(n // 2):
             for j in range((n + 1) // 2):
-                matrix[i][j], matrix[n -
-                                     j -
-                                     1][i], matrix[n -
-                                                   i -
-                                                   1][n -
-                                                      j -
-                                                      1], matrix[j][n -
-                                                                    i -
-                                                                    1] = matrix[j][n -
-                                                                                   i -
-                                                                                   1], matrix[i][j], matrix[n -
-                                                                                                            j -
-                                                                                                            1][i], matrix[n -
-                                                                                                                          i -
-                                                                                                                          1][n -
-                                                                                                                             j -
-                                                                                                                             1]
+                a, b, c, d = matrix[j][n - i - 1], matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1]
+                matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1] = a, b ,c ,d
 
         return matrix
 
