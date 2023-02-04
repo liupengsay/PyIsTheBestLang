ce57bdf10bac3db0a7c9484da2cfd3d0addd30f1
liupengsay <liupengsay@foxmail.com>
2023-02-04 11:23:28 +0800

    双指针

--- algorithm/src/basis/two_pointer.py
+++ algorithm/src/basis/two_pointer.py
@@ -2,21 +2,25 @@ import unittest
 
 """
 算法：双指针、快慢指针
-功能：通过相对移动来减少计算复杂度
-题目：xx（xx）
-L2444 统计定界子数组的数目（https://leetcode.cn/problems/count-subarrays-with-fixed-bounds/）通向双指针的移动来根据两个指针的位置来进行计数
-L2398 预算内的最多机器人数目（https://leetcode.cn/problems/maximum-number-of-robots-within-budget/）同向双指针移动的条件限制有两个需要用有序集合来维护滑动窗口过程
-L2302 统计得分小于 K 的子数组数目（https://leetcode.cn/problems/count-subarrays-with-score-less-than-k/）同向双指针维护指针位置与计数
-L2301 替换字符后匹配（https://leetcode.cn/problems/match-substring-after-replacement/）枚举匹配字符起点并使用双指针维护可行长度
-L2106 摘水果（https://leetcode.cn/problems/maximum-fruits-harvested-after-at-most-k-steps/）巧妙利用行走距离的计算更新双指针
-P2207 Photo（https://www.luogu.com.cn/problem/P2207）贪心加同向双指针
+功能：通过相对移动，来减少计算复杂度，分为同向双指针，相反双指针，以及中心扩展法
 
-P2381 圆圆舞蹈（https://www.luogu.com.cn/problem/P2381）环形数组，滑动窗口双指针
-6293. 统计好子数组的数目（https://leetcode.cn/problems/count-the-number-of-good-subarrays/）双指针计数
+题目：
+
+===================================力扣===================================
 259. 较小的三数之和（https://leetcode.cn/problems/3sum-smaller/）使用双指针或者计数枚举的方式
+2444. 统计定界子数组的数目（https://leetcode.cn/problems/count-subarrays-with-fixed-bounds/）通向双指针的移动来根据两个指针的位置来进行计数
+2398. 预算内的最多机器人数目（https://leetcode.cn/problems/maximum-number-of-robots-within-budget/）同向双指针移动的条件限制有两个需要用有序集合来维护滑动窗口过程
+2302. 统计得分小于 K 的子数组数目（https://leetcode.cn/problems/count-subarrays-with-score-less-than-k/）同向双指针维护指针位置与计数
+2301. 替换字符后匹配（https://leetcode.cn/problems/match-substring-after-replacement/）枚举匹配字符起点并使用双指针维护可行长度
+2106. 摘水果（https://leetcode.cn/problems/maximum-fruits-harvested-after-at-most-k-steps/）巧妙利用行走距离的计算更新双指针
+6293. 统计好子数组的数目（https://leetcode.cn/problems/count-the-number-of-good-subarrays/）双指针计数
+
+===================================洛谷===================================
+P2381 圆圆舞蹈（https://www.luogu.com.cn/problem/P2381）环形数组，滑动窗口双指针
 P3353 在你窗外闪耀的星星（https://www.luogu.com.cn/problem/P3353）滑动窗口加双指针
 P3662 [USACO17FEB]Why Did the Cow Cross the Road II S（https://www.luogu.com.cn/problem/P3662）滑动子数组和
 P4995 跳跳！（https://www.luogu.com.cn/problem/P4995）排序后利用贪心与双指针进行模拟
+P2207 Photo（https://www.luogu.com.cn/problem/P2207）贪心加同向双指针
 
 参考：OI WiKi（xx）
 """
@@ -39,7 +43,7 @@ class TwoPointer:
 
     @staticmethod
     def same_direction(nums):
-        # 相同方向双指针（寻找最长不含重复元素的子序列）
+        # 模板: 相同方向双指针（寻找最长不含重复元素的子序列）
         n = len(nums)
         ans = j = 0
         pre = set()
@@ -55,7 +59,7 @@ class TwoPointer:
 
     @staticmethod
     def opposite_direction(nums, target):
-        # 相反方向双指针（寻找升序数组是否存在两个数和为target）
+        # 模板: 相反方向双指针（寻找升序数组是否存在两个数和为target）
         n = len(nums)
         i, j = 0, n - 1
         while i < j:
