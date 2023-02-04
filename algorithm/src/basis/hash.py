269891551c998fd25fcc2b7ae28f4fbb35a524dd
liupengsay <liupengsay@foxmail.com>
2023-02-04 11:03:30 +0800

    哈希

--- algorithm/src/basis/hash.py
+++ algorithm/src/basis/hash.py
@@ -1,45 +1,22 @@
-"""
+import unittest
 
-"""
 """
 算法：哈希
 功能：前后缀计数、索引、加和
 题目：
-L2143 在两个数组的区间中选取数字（https://leetcode.cn/problems/choose-numbers-from-two-arrays-in-range/）前缀和哈希计数转换为求连续子数组和为 0 的个数
+
+===================================力扣===================================
+2143. 在两个数组的区间中选取数字（https://leetcode.cn/problems/choose-numbers-from-two-arrays-in-range/）前缀和哈希计数转换为求连续子数组和为 0 的个数
+
+===================================洛谷===================================
 P2697 宝石串（https://www.luogu.com.cn/problem/P2697）哈希记录前缀和与对应索引
 P1114 “非常男女”计划（https://www.luogu.com.cn/record/list?user=739032&status=12&page=13）哈希记录前缀和与对应索引
+
 参考：OI WiKi（xx）
 """
 
-import bisect
-import random
-import re
-import unittest
 
-from typing import List
-import heapq
-import math
-from collections import defaultdict, Counter, deque
-from functools import lru_cache
-from itertools import combinations
-from sortedcontainers import SortedList, SortedDict, SortedSet
-
-from sortedcontainers import SortedDict
-from functools import reduce
-from operator import xor
-from functools import lru_cache
-
-import random
-from itertools import permutations, combinations
-import numpy as np
-
-from decimal import Decimal
-
-import heapq
-import copy
-
-
-class ClassName:
+class HashMap:
     def __init__(self):
         return
 
@@ -49,9 +26,8 @@ class ClassName:
 
 class TestGeneral(unittest.TestCase):
 
-    def test_xxx(self):
-        nt = ClassName()
-        assert nt.gen_result(10 ** 11 + 131) == 66666666752
+    def test_hash_map(self):
+        pass
         return
 
 
