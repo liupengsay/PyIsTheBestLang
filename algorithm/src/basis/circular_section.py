787d346e0b161e954bc3391b0e85aa6b8dc05807
liupengsay <liupengsay@foxmail.com>
2023-02-04 10:43:33 +0800

    循环节计算

--- algorithm/src/basis/circular_section.py
+++ algorithm/src/basis/circular_section.py
@@ -4,11 +4,14 @@ import unittest
 算法：循环节
 功能：通过模拟找出循环节进行状态计算
 题目：
-Lxxxx xxxx（https://leetcode.cn/problems/shortest-palindrome/）xxxx
-P1965 [NOIP2013 提高组] 转圈游戏（https://www.luogu.com.cn/problem/P1965）循环节计算
+
+===================================力扣===================================
 957. N 天后的牢房（https://leetcode.cn/problems/prison-cells-after-n-days/）循环节计算
 418. 屏幕可显示句子的数量（https://leetcode.cn/problems/sentence-screen-fitting/）循环节计算
 466. 统计重复个数（https://leetcode.cn/problems/count-the-repetitions/）循环节计算
+
+===================================洛谷===================================
+P1965 [NOIP2013 提高组] 转圈游戏（https://www.luogu.com.cn/problem/P1965）循环节计算
 P1532 卡布列克圆舞曲（https://www.luogu.com.cn/problem/P1532）循环节计算
 P2203 Blink（https://www.luogu.com.cn/problem/P2203）循环节计算
 P5550 Chino的数列（https://www.luogu.com.cn/problem/P5550）循环节计算也可以使用矩阵快速幂递推
@@ -23,10 +26,11 @@ class CircleSection:
         return
 
     @staticmethod
-    def compute_circle_result(n, m, x, tm):  # 循环节模板
+    def compute_circle_result(n, m, x, tm):
 
-        # 计算 x 每次加 m 加了 m 次后模 n 的循环态
+        # 模板: 使用哈希与列表模拟记录循环节开始位置
         dct = dict()
+        # 计算 x 每次加 m 加了 tm 次后模 n 的循环态
         lst = []
         while x not in dct:
             dct[x] = len(lst)
@@ -51,9 +55,8 @@ class CircleSection:
 
 class TestGeneral(unittest.TestCase):
 
-    def test_xxx(self):
-        # nt = ClassName()
-        # assert nt.gen_result(1 0* *11 + 131) == 66666666752
+    def test_circle_section(self):
+        pass
         return
 
 
