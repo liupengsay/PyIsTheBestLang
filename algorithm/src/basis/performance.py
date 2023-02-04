7cae731b8563a722d3a0aeda7e2ec2af876f3bed
liupengsay <liupengsay@foxmail.com>
2023-02-04 11:09:11 +0800

    语言技巧

--- algorithm/src/basis/performance.py
+++ algorithm/src/basis/performance.py
@@ -1,43 +1,19 @@
-
-import bisect
-import random
-import re
 import unittest
-
-from typing import List
-import heapq
-import math
-from collections import defaultdict, Counter, deque
-from functools import lru_cache
-from itertools import combinations
-from sortedcontainers import SortedList, SortedDict, SortedSet
-
-from sortedcontainers import SortedDict
-from functools import reduce
-from operator import xor
-from functools import lru_cache
-
-import random
-from itertools import permutations, combinations
-import numpy as np
-
-from decimal import Decimal
-
-import heapq
-import copy
 
 """
 算法：性能
 功能：一些特殊的写法技巧用于加速
 题目：
+
+
+===================================洛谷===================================
 P1188 PASTE（https://www.luogu.com.cn/problem/P1188）切片的删除与赋值
 
 参考：OI WiKi（xx）
 """
 
 
-
-class ClassName:
+class Performance:
     def __init__(self):
         return
 
@@ -45,10 +21,10 @@ class ClassName:
         return
 
 
-
 class TestGeneral(unittest.TestCase):
 
-    def test_xxx(self):
+    def test_performance(self):
+        pass
         return
 
 
