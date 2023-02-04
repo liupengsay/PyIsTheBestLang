fbec351dda275d4698074a11130828cf0012e269
liupengsay <liupengsay@foxmail.com>
2023-02-04 10:45:02 +0800

    构造类题目

---
+++ algorithm/src/basis/construction.py
@@ -0,0 +1,36 @@
+import unittest
+
+"""
+
+算法：构造
+功能：通常需要根据题目要求，进行贪心排序之后的构造出合理可行的答案
+题目：xx（xx）
+
+===================================力扣===================================
+280. 摆动排序（https://leetcode.cn/problems/wiggle-sort/）按照降序先赋给奇数索引再给偶数索引
+
+===================================洛谷===================================
+P8846 『JROI-7』PMK 配匹串符字（https://www.luogu.com.cn/problem/P8846）厘清题意进行贪心构造
+P2902 [USACO08MAR]Pearl Pairing G（https://www.luogu.com.cn/problem/P2902）构造不同颜色对
+P5823 【L&K R-03】课表的排列（https://www.luogu.com.cn/problem/P5823）构造相同元素间隔为等差1的数组
+参考：OI WiKi（xx）
+"""
+
+
+class Construction:
+    def __init__(self):
+        return
+
+    def gen_result(self):
+        return
+
+
+class TestGeneral(unittest.TestCase):
+
+    def test_construction(self):
+        pass
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
