9672d30b963e41418d37579e51581939eec750da
liupengsay <liupengsay@foxmail.com>
2023-02-04 12:00:43 +0800

    优先队列，滑动窗口最大值最小值

--- algorithm/src/data_structure/priority_queue.py
+++ algorithm/src/data_structure/priority_queue.py
@@ -1,43 +1,25 @@
+import unittest
+
+from collections import deque
+
+import unittest
+from collections import deque
+
 """
 算法：单调队列、双端队列
 功能：维护单调性，计算滑动窗口最大值最小值
 题目：
+
+===================================力扣===================================
+239. 滑动窗口最大值（https://leetcode.cn/problems/sliding-window-maximum/）滑动区间最大值
+
+===================================洛谷===================================
 P2251 质量检测（https://www.luogu.com.cn/problem/P2251）滑动区间最小值
-L0239 滑动窗口最大值（https://leetcode.cn/problems/sliding-window-maximum/）滑动区间最大值
 P1750 出栈序列（https://www.luogu.com.cn/problem/P1750）经典题目，滑动指针窗口栈加队列
-
 P2311 loidc，想想看（https://www.luogu.com.cn/problem/P2311）不定长滑动窗口最大值索引
+
 参考：OI WiKi（xx）
-
-
 """
-
-import bisect
-import random
-import re
-import unittest
-
-from typing import List
-import heapq
-import math
-from collections import defaultdict, Counter, deque
-from functools import lru_cache
-from itertools import combinations
-from sortedcontainers import SortedList, SortedDict, SortedSet
-
-from sortedcontainers import SortedDict
-from functools import reduce
-from operator import xor
-from functools import lru_cache
-
-import random
-from itertools import permutations, combinations
-import numpy as np
-
-from decimal import Decimal
-
-import heapq
-import copy
 
 
 class PriorityQueue:
@@ -47,7 +29,7 @@ class PriorityQueue:
     @staticmethod
     def sliding_window(nums, k, method="max"):
         assert k >= 1
-        # 计算滑动窗口最大值与最小值
+        # 模板: 计算滑动窗口最大值与最小值
         if method == "min":
             nums = [-num for num in nums]
         n = len(nums)
