7021580a0f8c89be03ec541516427bf4928c4191
liupengsay <liupengsay@foxmail.com>
2023-02-05 17:05:52 +0800

    区间DP

--- algorithm/src/dp/interval_dp.py
+++ algorithm/src/dp/interval_dp.py
@@ -1,3 +1,5 @@
+
+import unittest
 from typing import List
 
 MOD = 10 ** 9 + 7
@@ -5,29 +7,32 @@ MOD = 10 ** 9 + 7
 
 """
 算法：区间DP
-功能：xxx
+功能：前缀和优化区间DP（需要在状态转移的时候更新代价距离）、预处理区间DP（需要预处理一个DP再计算最终DP）
 题目：
 
-前缀和优化区间DP（需要在状态转移的时候更新代价距离）
-L2478 完美分割的方案数（https://leetcode.cn/problems/number-of-beautiful-partitions/）
-L2463 最小移动总距离（https://leetcode.cn/problems/minimum-total-distance-traveled/）
+===================================力扣===================================
+2478. 完美分割的方案数（https://leetcode.cn/problems/number-of-beautiful-partitions/）
+2463. 最小移动总距离（https://leetcode.cn/problems/minimum-total-distance-traveled/）
+2472. 不重叠回文子字符串的最大数目（https://leetcode.cn/problems/maximum-number-of-non-overlapping-palindrome-substrings/）回文子串判定DP加线性DP
+2430. 对字母串可执行的最大删除数（https://leetcode.cn/problems/maximum-deletions-on-a-string/）最长公共前缀DP加线性DP
+1547. 切棍子的最小成本（https://leetcode.cn/problems/minimum-cost-to-cut-a-stick/）区间DP模拟
 
-预处理区间DP（需要预处理一个DP再计算最终DP）
-L2472 不重叠回文子字符串的最大数目（https://leetcode.cn/problems/maximum-number-of-non-overlapping-palindrome-substrings/）回文子串判定DP加线性DP
-L2430 对字母串可执行的最大删除数（https://leetcode.cn/problems/maximum-deletions-on-a-string/）最长公共前缀DP加线性DP
+===================================洛谷===================================
 P1521 求逆序对（https://www.luogu.com.cn/problem/P1521）使用归并排序计算移动次数，也可以使用倍增的树状数组
 P1775 石子合并（弱化版）（https://www.luogu.com.cn/problem/P1775）典型区间DP和前缀和预处理
 P2426 删数（https://www.luogu.com.cn/problem/P2426）典型区间DP
 P2690 [USACO04NOV]Apple Catching G（https://www.luogu.com.cn/problem/P2690）区间DP记忆化搜索模拟
 P1435 [IOI2000] 回文字串（https://www.luogu.com.cn/problem/P1435）典型区间DP求最长不连续回文子序列
-
 P1388 算式（https://www.luogu.com.cn/problem/P1388）回溯枚举符号组合，再使用区间DP进行最大值求解
 P1103 书本整理（https://www.luogu.com.cn/problem/P1103）三维DP
-1547. 切棍子的最小成本（https://leetcode.cn/problems/minimum-cost-to-cut-a-stick/）区间DP模拟
 P2858 [USACO06FEB]Treats for the Cows G/S（https://www.luogu.com.cn/problem/P2858）典型区间DP
 参考：OI WiKi（xx）
 """
 
+
+class IntervalDP:
+    def __init__(self):
+        return
 
 
 class Solution:
@@ -99,3 +104,16 @@ class Solution:
                 if i - j + 1 >= k and res[j][i]:
                     dp[i + 1] = max(dp[i + 1], dp[j] + 1)
         return dp[-1]
+
+
+class TestGeneral(unittest.TestCase):
+
+    def test_linear_dp(self):
+        ld = LinearDP()
+        nums = [6, 3, 5, 2, 1, 6, 8, 9]
+        assert ld.liner_dp_template(nums) == 4
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
