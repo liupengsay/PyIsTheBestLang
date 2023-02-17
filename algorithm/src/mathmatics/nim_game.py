7020a03c42715f5a7a1add7d5e99926ee6b42c7a
liupengsay <liupengsay@foxmail.com>
2023-02-17 08:45:33 +0800

    博弈贪心，使用大顶堆优先选取最大数量的石头做选择

--- algorithm/src/mathmatics/nim_game.py
+++ algorithm/src/mathmatics/nim_game.py
@@ -1,6 +1,6 @@
 
 import unittest
-
+import heapq
 from functools import reduce
 from operator import xor
 
@@ -12,10 +12,34 @@ from operator import xor
 
 ===================================洛谷===================================
 P2197 【模板】nim 游戏（https://www.luogu.com.cn/problem/P2197）有一个神奇的结论：当n堆石子的数量异或和等于0时，先手必胜，否则先手必败
+
+================================CodeForces================================
+B. Stoned Game（https://codeforces.com/problemset/problem/1396/B）博弈贪心，使用大顶堆优先选取最大数量的石头做选择
+
 参考：OI WiKi（https://oi-wiki.org/graph/lgv/）
 
-
 """
+
+
+class Solution:
+    def __init__(self):
+        return
+
+    @staticmethod
+    def cf_1396b(nums):
+        # 模板：博弈贪心，使用大顶堆优先选取最大数量的石头做选择
+        heapq.heapify(nums)
+        order = 0
+        pre = 0
+        while nums:
+            num = -heapq.heappop(nums)
+            num -= 1
+            tmp = num
+            if pre:
+                heapq.heappush(nums, -pre)
+            pre = tmp
+            order = 1 - order
+        return "HL" if not order else "T"
 
 
 class Nim:
@@ -30,7 +54,7 @@ class Nim:
 class TestGeneral(unittest.TestCase):
     def test_euler_phi(self):
         nim = Nim([0, 2, 3])
-        assert nim.gen_result() == True
+        assert nim.gen_result()
         return
 
 
