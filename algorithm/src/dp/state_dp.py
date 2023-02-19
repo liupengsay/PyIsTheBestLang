a49ac2ab4afe2bca7f8131599f6dce95cb0f5bb0
liupengsay <liupengsay@foxmail.com>
2023-02-20 07:54:35 +0800

    bitmask位运算状态压缩转移，从 1 少的状态向多的转移，并枚举前一个 1 的位置计算增益

--- algorithm/src/dp/state_dp.py
+++ algorithm/src/dp/state_dp.py
@@ -1,27 +1,3 @@
-"""
-
-"""
-"""
-算法：状态压缩DP
-功能：使用二进制数字表示转移状态，计算相应的转移方程，通常可以先计算满足条件的子集，有时通过深搜回溯枚举全部子集的办法比位运算枚举效率更高
-题目：
-
-===================================力扣===================================
-1723. 完成所有工作的最短时间（https://leetcode.cn/problems/find-minimum-time-to-finish-all-jobs/）通过位运算枚举分配工作DP最小化的最大值
-1986. 完成任务的最少工作时间段（https://leetcode.cn/problems/minimum-number-of-work-sessions-to-finish-the-tasks/）预处理计算子集后进行记忆化状态转移
-698. 划分为k个相等的子集（https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/）预处理计算子集后进行记忆化状态转移
-1349. 参加考试的最大学生数（https://leetcode.cn/problems/maximum-students-taking-exam/）按行状态枚举所有的摆放可能性
-2172. 数组的最大与和（https://leetcode.cn/problems/maximum-and-sum-of-array/）使用位运算和状态压缩进行转移
-
-===================================洛谷===================================
-P2704 炮兵阵地（https://www.luogu.com.cn/problem/P2704）记录两个前序状态进行转移
-P1896 互不侵犯（https://www.luogu.com.cn/problem/P1896）按行状态与行个数枚举所有的摆放可能性
-P2196 [NOIP1996 提高组] 挖地雷（https://www.luogu.com.cn/problem/P2196）有向图最长路径加状压DP
-P1690 贪婪的Copy（https://www.luogu.com.cn/problem/P1690）最短路加状压DP
-P1294 高手去散步（https://www.luogu.com.cn/problem/P1294）图问题使用状压DP求解最长直径
-P1123 取数游戏（https://www.luogu.com.cn/problem/P1123）类似占座位的经典状压DP
-参考：OI WiKi（xx）
-"""
 
 
 
@@ -47,6 +23,68 @@ import numpy as np
 from decimal import Decimal
 import heapq
 import copy
+
+"""
+算法：状态压缩DP
+功能：使用二进制数字表示转移状态，计算相应的转移方程，通常可以先计算满足条件的子集，有时通过深搜回溯枚举全部子集的办法比位运算枚举效率更高
+题目：
+
+===================================力扣===================================
+1723. 完成所有工作的最短时间（https://leetcode.cn/problems/find-minimum-time-to-finish-all-jobs/）通过位运算枚举分配工作DP最小化的最大值
+1986. 完成任务的最少工作时间段（https://leetcode.cn/problems/minimum-number-of-work-sessions-to-finish-the-tasks/）预处理计算子集后进行记忆化状态转移
+698. 划分为k个相等的子集（https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/）预处理计算子集后进行记忆化状态转移
+1349. 参加考试的最大学生数（https://leetcode.cn/problems/maximum-students-taking-exam/）按行状态枚举所有的摆放可能性
+2172. 数组的最大与和（https://leetcode.cn/problems/maximum-and-sum-of-array/）使用位运算和状态压缩进行转移
+
+===================================洛谷===================================
+P2704 炮兵阵地（https://www.luogu.com.cn/problem/P2704）记录两个前序状态进行转移
+P1896 互不侵犯（https://www.luogu.com.cn/problem/P1896）按行状态与行个数枚举所有的摆放可能性
+P2196 [NOIP1996 提高组] 挖地雷（https://www.luogu.com.cn/problem/P2196）有向图最长路径加状压DP
+P1690 贪婪的Copy（https://www.luogu.com.cn/problem/P1690）最短路加状压DP
+P1294 高手去散步（https://www.luogu.com.cn/problem/P1294）图问题使用状压DP求解最长直径
+P1123 取数游戏（https://www.luogu.com.cn/problem/P1123）类似占座位的经典状压DP
+
+================================CodeForces================================
+D. Kefa and Dishes（https://codeforces.com/problemset/problem/580/D）状态压缩DP结合前后相邻的增益计算最优解
+参考：OI WiKi（xx）
+"""
+
+
+class Solution:
+    def __int__(self):
+        return
+
+    @staticmethod
+    def cf_580d(ac):
+
+        # 模板：bitmask位运算状态压缩转移，从 1 少的状态向多的转移，并枚举前一个 1 的位置计算增益
+        n, m, k = ac.read_ints()
+        ind = {1 << i: i for i in range(n + 1)}
+        nums = ac.read_list_ints()
+        dp = [[0] * (n + 1) for _ in range(1 << n)]
+        edge = [[0] * (n + 1) for _ in range(n + 1)]
+        for _ in range(k):
+            x, y, c = ac.read_ints()
+            x -= 1
+            y -= 1
+            edge[x][y] = c
+
+        ans = 0
+        for i in range(1, 1 << n):
+            if bin(i).count("1") > m:
+                continue
+            res = 0
+            mask = i
+            while mask:
+                j = ind[mask & (-mask)]
+                cur = max(dp[i ^ (1 << j)][k] + edge[k][j] for k in range(n) if i & (1 << k)) + nums[j]
+                res = ac.max(res, cur)
+                mask &= (mask - 1)
+                dp[i][j] = cur
+            if bin(i).count("1") == m:
+                ans = ac.max(ans, res)
+        ac.st(ans)
+        return
 
 
 class StateDP:
