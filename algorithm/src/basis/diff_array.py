0cd9bbb0b7ce8041a3466a8cdb9b9162e190e0e3
liupengsay <liupengsay@foxmail.com>
2023-02-21 22:37:38 +0800

    模板：使用单调栈确定加和范围，再使用前缀和的前缀和进行计数

--- algorithm/src/basis/diff_array.py
+++ algorithm/src/basis/diff_array.py
@@ -1,4 +1,6 @@
 import unittest
+from typing import List
+
 from algorithm.src.fast_io import FastIO
 import bisect
 
@@ -47,7 +49,93 @@ class DiffArray:
         return
 
     @staticmethod
+    def get_diff_array(n, shifts):
+        # 一维差分数组
+        diff = [0] * n
+        for i, j, d in shifts:
+            if j + 1 < n:
+                diff[j + 1] -= d
+            diff[i] += d
+        for i in range(1, n):
+            diff[i] += diff[i - 1]
+        return diff
+
+    @staticmethod
+    def get_array_prefix_sum(n, lst):
+        # 一维前缀和
+        pre = [0] * (n + 1)
+        for i in range(n):
+            pre[i + 1] = pre[i] + lst[i]
+        return pre
+
+    @staticmethod
+    def get_array_range_sum(pre, left, right):
+        # 区间元素和
+        return pre[right + 1] - pre[left]
+
+
+class DiffMatrix:
+    def __init__(self):
+        return
+
+    @staticmethod
+    def get_diff_matrix(m, n, shifts):
+        # 二维差分数组
+        diff = [[0] * (n + 2) for _ in range(m + 2)]
+        # 索引从 1 开始，矩阵初始值为 0
+        for xa, xb, ya, yb, d in shifts:  # 注意这里的行列索引范围，是从左上角到右下角
+            diff[xa][ya] += d
+            diff[xa][yb + 1] -= d
+            diff[xb + 1][ya] -= d
+            diff[xb + 1][yb + 1] += d
+
+        for i in range(1, m + 2):
+            for j in range(1, n + 2):
+                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1]
+
+        for i in range(1, m + 1):
+            diff[i] = diff[i][1:n + 1]
+        return diff[1: m + 1]
+
+    @staticmethod
+    def get_diff_matrix2(m, n, shifts):
+        diff = [[0] * (n + 1) for _ in range(m + 1)]
+        # 二维差分，索引从 0 开始， 注意这里的行列索引范围，是从左上角到右下角
+        for xa, xb, ya, yb, d in shifts:
+            diff[xa][ya] += d
+            diff[xa][yb + 1] -= d
+            diff[xb + 1][ya] -= d
+            diff[xb + 1][yb + 1] += d
+
+        res = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                res[i + 1][j + 1] = res[i + 1][j] + res[i][j + 1] - res[i][j] + diff[i][j]
+        return [item[1:] for item in res[1:]]
+
+    @staticmethod
+    def get_matrix_prefix_sum(mat):
+        # 二维前缀和
+        m, n = len(mat), len(mat[0])
+        pre = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                pre[i + 1][j + 1] = pre[i][j + 1] + pre[i + 1][j] - pre[i][j] + mat[i][j]
+        return pre
+
+    @staticmethod
+    def get_matrix_range_sum(pre, xa, ya, xb, yb):
+        # 二维子矩阵和
+        return pre[xb + 1][yb + 1] - pre[xb + 1][ya] - pre[xa][yb + 1] + pre[xa][ya]
+
+
+class Solution:
+    def __init__(self):
+        return
+
+    @staticmethod
     def cf_1795c(ac=FastIO()):
+        # 模板：根据作用域进行差分数组计数
         for _ in range(ac.read_int()):
             n = ac.read_int()
             a = ac.read_list_ints()
@@ -96,84 +184,39 @@ class DiffArray:
         return ans
 
     @staticmethod
-    def get_diff_array(n, shifts):
-        # 一维差分数组
-        diff = [0] * n
-        for i, j, d in shifts:
-            if j + 1 < n:
-                diff[j + 1] -= d
-            diff[i] += d
-        for i in range(1, n):
-            diff[i] += diff[i - 1]
-        return diff
+    def lc_2281(strength: List[int]) -> int:
+        # 模板：使用单调栈确定加和范围，再使用前缀和的前缀和进行计数
+        n = len(strength)
+        mod = 10 ** 9 + 7
 
-    @staticmethod
-    def get_array_prefix_sum(n, lst):
-        # 一维前缀和
-        pre = [0] * (n + 1)
+        # 单调栈
+        pre = [0] * n
+        post = [n - 1] * n
+        stack = []
         for i in range(n):
-            pre[i + 1] = pre[i] + lst[i]
-        return pre
+            while stack and strength[stack[-1]] >= strength[i]:
+                post[stack.pop()] = i - 1
+            if stack:
+                pre[i] = stack[-1] + 1
+            stack.append(i)
 
-    @staticmethod
-    def get_array_range_sum(pre, left, right):
-        # 区间元素和
-        return pre[right + 1] - pre[left]
+        # 前缀和
+        s = [0] * (n + 1)
+        for i in range(n):
+            s[i + 1] = s[i] + strength[i]
 
+        # 前缀和的前缀和
+        ss = [0] * (n + 2)
+        for i in range(n + 1):
+            ss[i + 1] = ss[i] + s[i]
 
-class DiffMatrix:
-    def __init__(self):
-        return
-
-    @staticmethod
-    def get_diff_matrix(m, n, shifts):
-        # 二维差分数组
-        diff = [[0] * (n + 2) for _ in range(m + 2)]
-        # 索引从 1 开始，矩阵初始值为 0
-        for xa, xb, ya, yb, d in shifts:  # 注意这里的行列索引范围，是从左上角到右下角
-            diff[xa][ya] += d
-            diff[xa][yb + 1] -= d
-            diff[xb + 1][ya] -= d
-            diff[xb + 1][yb + 1] += d
-
-        for i in range(1, m + 2):
-            for j in range(1, n + 2):
-                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1]
-
-        for i in range(1, m + 1):
-            diff[i] = diff[i][1:n + 1]
-        return diff[1: m+1]
-
-    @staticmethod
-    def get_diff_matrix2(m, n, shifts):
-        diff = [[0] * (n + 1) for _ in range(m + 1)]
-        # 二维差分，索引从 0 开始， 注意这里的行列索引范围，是从左上角到右下角
-        for xa, xb, ya, yb, d in shifts:
-            diff[xa][ya] += d
-            diff[xa][yb + 1] -= d
-            diff[xb + 1][ya] -= d
-            diff[xb + 1][yb + 1] += d
-
-        res = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m):
-            for j in range(n):
-                res[i + 1][j + 1] = res[i + 1][j] + res[i][j + 1] - res[i][j] + diff[i][j]
-        return [item[1:] for item in res[1:]]
-
-    @staticmethod
-    def get_matrix_prefix_sum(mat):
-        # 二维前缀和
-        m, n = len(mat), len(mat[0])
-        pre = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m):
-            for j in range(n):
-                pre[i + 1][j + 1] = pre[i][j + 1] + pre[i + 1][j] - pre[i][j] + mat[i][j]
-        return pre
-
-    @staticmethod
-    def get_matrix_range_sum(pre, xa, ya, xb, yb):
-        # 二维子矩阵和
-        return pre[xb + 1][yb + 1] - pre[xb + 1][ya] - pre[xa][yb + 1] + pre[xa][ya]
+        # 遍历加和
+        ans = 0
+        for i in range(n):
+            left, right = pre[i], post[i]
+            ans += strength[i] * ((i - left + 1) * (ss[right + 2] - ss[i + 1]) - (right - i + 1) * (ss[i + 1] - ss[left]))
+            ans %= mod
+        return ans
 
 
 class TestGeneral(unittest.TestCase):
