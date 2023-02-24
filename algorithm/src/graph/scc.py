eb405d5786eb8a7d79cec149ed4d4cfd047a765a
liupengsay <liupengsay@foxmail.com>
2023-02-24 23:14:22 +0800

    模板：2-SAT 适定性问题（使用有向图scc强连通分量求解）

--- algorithm/src/graph/scc.py
+++ algorithm/src/graph/scc.py
@@ -3,13 +3,15 @@ import unittest
 
 from collections import defaultdict
 
+from algorithm.src.fast_io import FastIO
+
 """
 
 算法：强连通分量、2-SAT、最大环、最小环
 功能：用来求解有向图的强连通分量，可以将一张图的每个强连通分量都缩成一个点，然后这张图会变成一个 DAG，可以进行拓扑排序以及更多其他操作
 定义：有向图 G 强连通是指 G 中任意两个结点连通，强连通分量（Strongly Connected Components，SCC）是极大的强连通子图
 距离：求一条路径，可以经过重复结点，要求经过的不同结点数量最多
-
+2-SAT：简单的说就是给出 n 个集合，每个集合有两个元素，已知若干个 <a,b>，表示 a 与 b 矛盾（其中 a 与 b 属于不同的集合）。然后从每个集合选择一个元素，判断能否一共选 n 个两两不矛盾的元素。显然可能有多种选择方案，一般题中只需要求出一种即可。
 题目：
 
 ===================================力扣===================================
@@ -20,10 +22,10 @@ P3387 缩点 （https://www.luogu.com.cn/problem/solution/P3387）允许多次�
 P2661 [NOIP2015 提高组] 信息传递（https://www.luogu.com.cn/problem/P2661）求最小的环长度（有向图、内向基环树没有环套环，N个节点N条边，也可以使用拓扑排序）
 P4089 [USACO17DEC]The Bovine Shuffle S（https://www.luogu.com.cn/problem/P4089）求所有环的长度和，注意自环
 P5145 漂浮的鸭子（https://www.luogu.com.cn/problem/P5145）内向基环树求最大权值和的环
+
 P4782 【模板】2-SAT 问题（https://www.luogu.com.cn/problem/P4782）2-SAT 问题模板题
 P5782 [POI2001] 和平委员会（https://www.luogu.com.cn/problem/P5782）2-SAT 问题模板题
 P4171 [JSOI2010] 满汉全席（https://www.luogu.com.cn/problem/P4171）2-SAT 问题模板题
-
 ================================CodeForces================================
 C. Engineer Artem（https://codeforces.com/problemset/problem/1438/C）2-SAT 问题模板题
 
@@ -167,54 +169,62 @@ class TwoSAT:
         return
 
     @staticmethod
-    def cf_1438c(m, n, grid):
-        # 建图并把索引编码
-        edge = [[] for _ in range(2 * m * n)]
-        for i in range(m):
-            for j in range(n):
-                if i + 1 < m:
-                    x, y = i * n + j, i * n + n + j
-                    for a, b in [[0, 0], [0, 1], [1, 0], [1, 1]]:
-                        if grid[i][j] + a == grid[i + 1][j] + b:
-                            edge[x * 2 + a].append(y * 2 + 1 - b)
-                            edge[y * 2 + b].append(x * 2 + 1 - a)
-                if j + 1 < n:
-                    x, y = i * n + j, i * n + 1 + j
-                    for a, b in [[0, 0], [0, 1], [1, 0], [1, 1]]:
-                        if grid[i][j] + a == grid[i][j + 1] + b:
-                            edge[x * 2 + a].append(y * 2 + 1 - b)
-                            edge[y * 2 + b].append(x * 2 + 1 - a)
+    def cf_1438c(ac=FastIO()):
+        for _ in range(ac.read_int()):
+            m, n = ac.read_ints()
+            grid = [ac.read_list_ints() for _ in range(m)]
+            # 建图并把索引编码
+            edge = [[] for _ in range(2 * m * n)]
+            for i in range(m):
+                for j in range(n):
+                    if i + 1 < m:
+                        x, y = i * n + j, i * n + n + j
+                        for a, b in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+                            if grid[i][j] + a == grid[i + 1][j] + b:
+                                edge[x * 2 + a].append(y * 2 + 1 - b)
+                                edge[y * 2 + b].append(x * 2 + 1 - a)
+                    if j + 1 < n:
+                        x, y = i * n + j, i * n + 1 + j
+                        for a, b in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+                            if grid[i][j] + a == grid[i][j + 1] + b:
+                                edge[x * 2 + a].append(y * 2 + 1 - b)
+                                edge[y * 2 + b].append(x * 2 + 1 - a)
 
-        #####################################################
-        # 按照强连通进行缩点
-        tarjan = Tarjan(edge)
-        # 进行方案赋予，先出现的确定值
-        ans = [0] * m*n
-        pre = set()
-        for sc in tarjan.scc:
-            for node in sc:
-                i = node // 2
-                if i not in pre:
-                    ans[i] = node % 2
-                pre.add(i)
+            #####################################################
+            # 按照强连通进行缩点
+            tarjan = Tarjan(edge)
+            # 进行方案赋予，先出现的确定值
+            ans = [0] * m*n
+            pre = set()
+            for sc in tarjan.scc:
+                for node in sc:
+                    i = node // 2
+                    if i not in pre:
+                        ans[i] = node % 2
+                    pre.add(i)
 
-        for x in range(m * n):
-            grid[x // n][x % n] += ans[x]
-
-        #####################################################
-        # Kosaraju算法
-        kosaraju = Kosaraju(2 * m * n, edge)
-        # 注意是小于符号
-        ans = [int(kosaraju.color[2 * i] < kosaraju.color[2 * i + 1]) for i in range(m * n)]
-        for x in range(m * n):
-            grid[x // n][x % n] += ans[x]
-        return grid
+            for x in range(m * n):
+                grid[x // n][x % n] += ans[x]
+            for g in grid:
+                ac.lst(g)
+            #####################################################
+            # Kosaraju算法
+            kosaraju = Kosaraju(2 * m * n, edge)
+            # 注意是小于符号
+            ans = [int(kosaraju.color[2 * i] < kosaraju.color[2 * i + 1]) for i in range(m * n)]
+            for x in range(m * n):
+                grid[x // n][x % n] += ans[x]
+            for g in grid:
+                ac.lst(g)
+            return grid
 
     @staticmethod
-    def luogu_4782(n, pairs):
+    def luogu_4782(ac=FastIO()):
+        n, m = ac.read_ints()
         # 建图并把索引编码
         edge = [[] for _ in range(2 * n)]
-        for i, a, j, b in pairs:
+        for _ in range(m):
+            i, a, j, b = ac.read_ints()
             i -= 1
             j -= 1
             edge[i * 2 + 1 - a].append(j * 2 + b)
@@ -228,12 +238,12 @@ class TwoSAT:
             for node in sc:
                 # 条件相反的两个点不能在一个强连通分量
                 if node // 2 in pre:
-                    # ac.st("IMPOSSIBLE")
+                    ac.st("IMPOSSIBLE")
                     return
                 pre.add(node // 2)
 
         # 进行方案赋予，先出现的确定值
-        # ac.st("POSSIBLE")
+        ac.st("POSSIBLE")
         ans = [0] * n
         pre = set()
         for sc in tarjan.scc:
@@ -242,28 +252,29 @@ class TwoSAT:
                 if i not in pre:
                     ans[i] = node % 2
                 pre.add(i)
-        # ac.lst(ans)
+        ac.lst(ans)
 
         #####################################################
         # Kosaraju算法（与上面的算法二选一）
         kosaraju = Kosaraju(2 * n, edge)
         for i in range(n):
             if kosaraju.color[i * 2] == kosaraju.color[i * 2 + 1]:
-                # ac.st("IMPOSSIBLE")
+                ac.st("IMPOSSIBLE")
                 return
 
-        # ac.st("POSSIBLE")
+        ac.st("POSSIBLE")
         ans = [int(kosaraju.color[2 * i] < kosaraju.color[2 * i + 1])
                for i in range(n)]
-        # ac.lst(ans)
+        ac.lst(ans)
         return
 
     @staticmethod
-    def luogu_p5782(n, pairs):
-
+    def luogu_p5782(ac=FastIO()):
+        n, m = ac.read_ints()
         # 建图并把索引编码
         edge = [[] for _ in range(4 * n)]
-        for a, b in pairs:
+        for _ in range(m):
+            a, b = ac.read_ints_minus_one()
             edge[a * 2 + 1].append(b * 2)
             edge[b * 2 + 1].append(a * 2)
 
@@ -283,7 +294,7 @@ class TwoSAT:
             for node in sc:
                 # 条件相反的两个点不能在一个强连通分量
                 if node // 2 in pre:
-                    # ac.st("NIE")
+                    ac.st("NIE")
                     return
                 pre.add(node // 2)
 
@@ -297,53 +308,64 @@ class TwoSAT:
                     ans[i] = node % 2
                 pre.add(i)
         res = [i+1 for i in range(2 * n) if ans[i]]
-
+        for a in res:
+            ac.st(a)
         #####################################################
         # Kosaraju算法
         kosaraju = Kosaraju(4 * n, edge)
         for i in range(2 * n):
             if kosaraju.color[i * 2] == kosaraju.color[i * 2 + 1]:
-                # ac.st("NIE")
+                ac.st("NIE")
                 return
         # 注意是小于符号
         ans = [int(kosaraju.color[2 * i] < kosaraju.color[2 * i + 1]) for i in range(2 * n)]
         res = [i + 1 for i in range(2 * n) if ans[i]]
+        for a in res:
+            ac.st(a)
         return
 
     @staticmethod
-    def luogu_4171(n, pairs):
-        # 建图并把索引编码
-        edge = [[] for _ in range(2 * n)]
-        for lst in pairs:
-            i = int(lst[0][1:]) - 1
-            j = int(lst[1][1:]) - 1
-            a = 1 if lst[0][0] == "h" else 0
-            b = 1 if lst[1][0] == "h" else 0
-            edge[i * 2 + (1 - a)].append(j * 2 + b)
-            edge[j * 2 + (1 - b)].append(i * 2 + a)
+    def luogu_4171(ac=FastIO()):
+        for _ in range(ac.read_int()):
+            n, m = ac.read_ints()
+            # 建图并把索引编码
+            edge = [[] for _ in range(2 * n)]
+            for _ in range(m):
+                lst = ac.read_list_strs()
+                i = int(lst[0][1:]) - 1
+                j = int(lst[1][1:]) - 1
+                a = 1 if lst[0][0] == "h" else 0
+                b = 1 if lst[1][0] == "h" else 0
+                edge[i * 2 + (1 - a)].append(j * 2 + b)
+                edge[j * 2 + (1 - b)].append(i * 2 + a)
 
-        #####################################################
-        # # 按照强连通进行缩点
-        tarjan = Tarjan(edge)
-        ans = True
-        for sc in tarjan.scc:
-            pre = set()
-            for node in sc:
-                # 条件相反的两个点不能在一个强连通分量
-                if node // 2 in pre:
+            #####################################################
+            # # 按照强连通进行缩点
+            tarjan = Tarjan(edge)
+            ans = True
+            for sc in tarjan.scc:
+                pre = set()
+                for node in sc:
+                    # 条件相反的两个点不能在一个强连通分量
+                    if node // 2 in pre:
+                        ans = False
+                    pre.add(node // 2)
+            ac.st("GOOD" if ans else "BAD")
+
+            #####################################################
+            # Kosaraju算法
+            kosaraju = Kosaraju(2 * n, edge)
+            ans = True
+            for i in range(n):
+                if kosaraju.color[i * 2] == kosaraju.color[i * 2 + 1]:
                     ans = False
-                pre.add(node // 2)
-        # ac.st("GOOD" if ans else "BAD")
+                    break
+            ac.st("GOOD" if ans else "BAD")
+        return
 
-        #####################################################
-        # Kosaraju算法
-        kosaraju = Kosaraju(2 * n, edge)
-        ans = True
-        for i in range(n):
-            if kosaraju.color[i * 2] == kosaraju.color[i * 2 + 1]:
-                ans = False
-                break
-        # ac.st("GOOD" if ans else "BAD")
+
+class Solution:
+    def __init__(self):
         return
 
 
