ae7fc332bd03689617d5607d9f83ce7d98e80529
liupengsay <liupengsay@foxmail.com>
2023-04-29 12:08:04 +0800

    序列化与反序列化

---
+++ algorithm/src/basis/serialization.py
@@ -0,0 +1,55 @@
+import math
+import random
+import unittest
+from functools import reduce
+from math import gcd
+from operator import add
+from itertools import accumulate
+from typing import List
+from operator import mul, add, xor, and_, or_
+from algorithm.src.fast_io import FastIO
+
+"""
+算法：序列化与反序列化
+功能：将二叉树、N叉树等数据结构序列化为字符串，并通过字符串反序列化恢复数据
+参考：
+题目：
+
+===================================力扣===================================
+428. 序列化和反序列化 N 叉树（https://leetcode.cn/problems/serialize-and-deserialize-n-ary-tree/）序列化模板题
+297. 二叉树的序列化与反序列化（https://leetcode.cn/problems/serialize-and-deserialize-binary-tree/）序列化模板题
+
+
+===================================洛谷===================================
+xx（xxx）xxx
+
+================================CodeForces================================
+xx（xxx）xxx
+
+"""
+
+
+class XXX:
+    def __init__(self):
+        return
+
+
+class Solution:
+    def __int__(self):
+        return
+
+    @staticmethod
+    def xx_xxxx(ac=FastIO()):
+        pass
+        return
+
+
+class TestGeneral(unittest.TestCase):
+
+    def test_xxxx(self):
+        pass
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
