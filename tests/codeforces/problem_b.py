0a9a6257aee60f5c6390707ce757c77f7cf1fe39
liupengsay <liupengsay@foxmail.com>
2023-08-13 22:20:03 +0800

    恢复模板

--- tests/codeforces/problem_b.py
+++ tests/codeforces/problem_b.py
@@ -1,5 +1,21 @@
-import random
+import bisect
+import decimal
+import heapq
+from types import GeneratorType
+from math import inf
 import sys
+from heapq import heappush, heappop, heappushpop
+from functools import cmp_to_key
+from collections import defaultdict, Counter, deque
+import math
+from functools import lru_cache
+from heapq import nlargest
+from functools import reduce
+import random
+from itertools import combinations, permutations
+from operator import xor, add
+from operator import mul
+from typing import List, Callable, Dict, Set, Tuple, DefaultDict
 
 
 class FastIO:
@@ -101,51 +117,13 @@ class FastIO:
         return random.randint(0, 10**9+7)
 
 
-class UnionFindRightRange:
-    # 模板：向右合并的并查集
-    def __init__(self, n: int) -> None:
-        self.root = [i for i in range(n)]
-        return
-
-    def find(self, x):
-        lst = []
-        while x != self.root[x]:
-            lst.append(x)
-            # 在查询的时候合并到顺带直接根节点
-            x = self.root[x]
-        for w in lst:
-            self.root[w] = x
-        return x
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if root_x == root_y:
-            return False
-        if root_x > root_y:
-            root_x, root_y = root_y, root_x
-        self.root[root_x] = root_y
-        return True
-
-
 class Solution:
     def __init__(self):
         return
 
     @staticmethod
-    def ac_4306(ac=FastIO()):
-        # 模板：经典向右合并的区间并查集
-        n = ac.read_int()
-        a = ac.read_list_ints()
-        uf = UnionFindRightRange(n*2+2)
-        a.sort()
-        ans = 0
-        for num in a:
-            # 其根节点就是当前还未被占据的节点
-            x = uf.find(num)
-            ans += x-num
-            uf.union(x, x+1)
-        ac.st(ans)
+    def main(ac=FastIO()):
+
         return
 
 
