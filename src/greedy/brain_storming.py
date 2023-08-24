fb6563f9bb6a5848be0dd127e1c605cddefdee1a
liupengsay <liupengsay@foxmail.com>
2023-08-24 08:19:32 +0800

    经典哈夫曼树Huffman Tree贪心从小到大合并，类似合并果子

--- src/greedy/brain_storming.py
+++ src/greedy/brain_storming.py
@@ -15,7 +15,7 @@ from src.mathmatics.number_theory import NumberTheory
 ===================================力扣===================================
 134. 加油站（https://leetcode.cn/problems/gas-station/）经典贪心
 330. 按要求补齐数组（https://leetcode.cn/problems/patching-array/）经典贪心
-1199. 建造街区的最短时间（https://leetcode.cn/problems/minimum-time-to-build-blocks/）经典哈夫曼树Huffman Tree贪心从小到大合并
+1199. 建造街区的最短时间（https://leetcode.cn/problems/minimum-time-to-build-blocks/）经典哈夫曼树Huffman Tree贪心从小到大合并，类似合并果子
 2499. 让数组不相等的最小总代价（https://leetcode.cn/problems/minimum-total-cost-to-make-arrays-unequal/）利用鸽巢原理贪心计算最小代价
 2449. 使数组相似的最少操作次数（https://leetcode.cn/problems/minimum-total-cost-to-make-arrays-unequal/）转换题意进行排序后用奇偶数贪心变换得到
 2448. 使数组相等的最小开销（https://leetcode.cn/problems/minimum-cost-to-make-array-equal/）利用中位数的特点变换到带权重广义下中位数的位置是最优的贪心进行增减
