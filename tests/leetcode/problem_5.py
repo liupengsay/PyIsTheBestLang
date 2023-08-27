6595684237a647bd48c5ba0c46b0696ddad6c884
liupengsay <liupengsay@foxmail.com>
2023-08-27 10:28:21 +0800

    增加一个备用

---
+++ tests/leetcode/problem_5.py
@@ -0,0 +1,50 @@
+import bisect
+import random
+import re
+import sys
+import unittest
+from typing import List, Callable
+from typing import List
+import heapq
+import math
+from collections import defaultdict, Counter, deque
+from functools import lru_cache, cmp_to_key
+from itertools import combinations, accumulate, chain
+from sortedcontainers import SortedList, SortedDict, SortedSet
+from sortedcontainers import SortedDict
+from functools import reduce
+from heapq import heappush, heappop, heappushpop
+from operator import xor, mul, add
+from functools import lru_cache
+from math import inf
+import random
+from itertools import permutations, combinations
+import numpy as np
+
+from decimal import Decimal
+
+import heapq
+import copy
+from sortedcontainers import SortedList
+
+# sys.set_int_max_str_digits(0)  # 大数的范围坑
+
+
+def max_(a, b):
+    return a if a > b else b
+
+
+def min_(a, b):
+    return a if a < b else b
+
+
+
+
+
+
+
+
+
+
+
+assert Solution()
