b36d63090a31249624973e5d1aa78fc9803705d6
liupengsay <liupengsay@foxmail.com>
2023-09-01 14:34:57 +0800

    最大连续子数组和变种

--- src/dp/linear_dp.py
+++ src/dp/linear_dp.py
@@ -35,6 +35,8 @@ from src.mathmatics.number_theory import NumberTheory
 2289. 使数组按非递减顺序排列（https://leetcode.cn/problems/steps-to-make-array-non-decreasing/description/）经典单调栈优化的线性DP，也可用BFS加链表求解
 2746. 字符串连接删减字母（https://leetcode.cn/problems/decremental-string-concatenation/）经典哈希线性DP模拟实现
 1911. 最大子序列交替和（https://leetcode.cn/problems/maximum-alternating-subsequence-sum/）经典线性DP
+2321. 拼接数组的最大分数（https://leetcode.cn/problems/maximum-score-of-spliced-array/description/）最大连续子数组和变种
+2320. 统计放置房子的方式数（https://leetcode.cn/problems/count-number-of-ways-to-place-houses/）经典线性DP
 
 ===================================洛谷===================================
 P1970 [NOIP2013 提高组] 花匠（https://www.luogu.com.cn/problem/P1970）使用贪心与动态规划计算最长的山脉子数组
