e374fb87489bce6e092aeacdb936aa92d4e5a76a
liupengsay <liupengsay@foxmail.com>
2023-09-01 12:08:26 +0800

    经典线性DP

--- src/dp/linear_dp.py
+++ src/dp/linear_dp.py
@@ -34,6 +34,7 @@ from src.mathmatics.number_theory import NumberTheory
 823. 带因子的二叉树（https://leetcode.cn/problems/binary-trees-with-factors/description/）经典线性DP计数
 2289. 使数组按非递减顺序排列（https://leetcode.cn/problems/steps-to-make-array-non-decreasing/description/）经典单调栈优化的线性DP，也可用BFS加链表求解
 2746. 字符串连接删减字母（https://leetcode.cn/problems/decremental-string-concatenation/）经典哈希线性DP模拟实现
+1911. 最大子序列交替和（https://leetcode.cn/problems/maximum-alternating-subsequence-sum/）经典线性DP
 
 ===================================洛谷===================================
 P1970 [NOIP2013 提高组] 花匠（https://www.luogu.com.cn/problem/P1970）使用贪心与动态规划计算最长的山脉子数组
