a85e93dc1defb0e7e465eacf30c9cb7f122a2673
liupengsay <liupengsay@foxmail.com>
2023-09-02 14:19:16 +0800

    经典容斥原理组合计数

--- tests/codeforces/problem_d.py
+++ tests/codeforces/problem_d.py
@@ -1,6 +1,22 @@
-import random
+import bisect
+import decimal
+import heapq
+from types import GeneratorType
+from math import inf
 import sys
-from collections import defaultdict
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heappushpop
+from functools import cmp_to_key
+from collections import defaultdict, Counter, deque
+import math
+from functools import lru_cache
+from heapq import nlargest
+from functools import reduce
+import random
+from itertools import combinations, permutations
+from operator import xor, add
+from operator import mul
+from typing import List, Callable, Dict, Set, Tuple, DefaultDict
 
 
 # sys.setrecursionlimit(10**8)设置最大递归次数
@@ -40,8 +56,7 @@ class FastIO:
 
     @staticmethod
     def read_list_ints_minus_one():
-        return list(map(lambda x: int(x) - 1,
-                        sys.stdin.readline().strip().split()))
+        return list(map(lambda x: int(x) - 1, sys.stdin.readline().strip().split()))
 
     @staticmethod
     def read_str():
@@ -110,96 +125,13 @@ class FastIO:
         return random.randint(0, 10 ** 9 + 7)
 
 
-class Combinatorics:
-    def __init__(self, n, mod):
-        # 模板：求全排列组合数，使用时注意 n 的取值范围
-        n += 10
-        self.perm = [1] * n
-        self.rev = [1] * n
-        self.mod = mod
-        for i in range(1, n):
-            # 阶乘数 i! 取模
-            self.perm[i] = self.perm[i - 1] * i
-            self.perm[i] %= self.mod
-        # 等价于pow(self.perm[-1], -1, self.mod)
-        self.rev[-1] = self.mod_reverse(self.perm[-1], self.mod)
-        for i in range(n - 2, 0, -1):
-            self.rev[i] = (self.rev[i + 1] * (i + 1) % mod)  # 阶乘 i! 取逆元
-        self.fault = [0] * n
-        self.fault_perm()
-        return
-
-    def ex_gcd(self, a, b):
-        # 扩展欧几里得求乘法逆元
-        if b == 0:
-            return 1, 0, a
-        else:
-            x, y, q = self.ex_gcd(b, a % b)
-            x, y = y, (x - (a // b) * y)
-            return x, y, q
-
-    def mod_reverse(self, a, p):
-        x, y, q = self.ex_gcd(a, p)
-        if q != 1:
-            raise Exception("No solution.")   # 逆元要求a与p互质
-        else:
-            return (x + p) % p  # 防止负数
-
-    def comb(self, a, b):
-        if a < b:
-            return 0
-        # 组合数根据乘法逆元求解
-        res = self.perm[a] * self.rev[b] * self.rev[a - b]
-        return res % self.mod
-
-    def factorial(self, a):
-        # 组合数根据乘法逆元求解
-        res = self.perm[a]
-        return res % self.mod
-
-    def fault_perm(self):
-        # 求错位排列组合数
-        self.fault[0] = 1
-        self.fault[2] = 1
-        for i in range(3, len(self.fault)):
-            self.fault[i] = (i - 1) * (self.fault[i - 1] + self.fault[i - 2])
-            self.fault[i] %= self.mod
-        return
-
-    def inv(self, n):
-        # 求 pow(n, -1, mod)
-        return self.perm[n - 1] * self.rev[n] % self.mod
-
-    def catalan(self, n):
-        # 求卡特兰数
-        return (self.comb(2 * n, n) - self.comb(2 * n, n - 1)) % self.mod
-
-
 class Solution:
     def __init__(self):
         return
 
     @staticmethod
-    def abc_65d(ac=FastIO()):
-        # 模板：经典容斥原理组合计数
-        mod = 10**9 + 7
-        n = ac.read_int()
-        nums = ac.read_list_ints()
-        ind = [-1, -1]
-        pre = defaultdict(list)
-        for i in range(n + 1):
-            pre[nums[i]].append(i)
-            if len(pre[nums[i]]) == 2:
-                ind = pre[nums[i]]
-                break
-        x = ind[0]
-        y = n - ind[-1]
-        cb = Combinatorics(n + 10, mod)
-        for k in range(1, n + 2):
-            ans = cb.comb(n + 1, k)
-            if 1 <= k <= x + y + 1:
-                ans -= cb.comb(x + y, k - 1)
-            ac.st(ans % mod)
+    def main(ac=FastIO()):
+
         return
 
 
