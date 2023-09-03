e874a444339231dbd9bb30f3d828b4cdcb4780a4
liupengsay <liupengsay@foxmail.com>
2023-09-04 06:23:47 +0800

    序列化模板题

--- src/basis/serialization.py
+++ src/basis/serialization.py
@@ -1,4 +1,8 @@
 import unittest
+from collections import deque
+from typing import Optional
+
+from src.basis.tree_node import TreeNode
 from src.fast_io import FastIO
 
 """
@@ -10,7 +14,7 @@ from src.fast_io import FastIO
 ===================================力扣===================================
 428. 序列化和反序列化 N 叉树（https://leetcode.cn/problems/serialize-and-deserialize-n-ary-tree/）序列化模板题
 297. 二叉树的序列化与反序列化（https://leetcode.cn/problems/serialize-and-deserialize-binary-tree/）序列化模板题
-
+449. 序列化和反序列化二叉搜索树（https://leetcode.cn/problems/serialize-and-deserialize-bst/）序列化模板题
 
 ===================================洛谷===================================
 xx（xxx）xxx
@@ -21,9 +25,75 @@ xx（xxx）xxx
 """
 
 
-class XXX:
-    def __init__(self):
-        return
+class CodecBFS:
+
+    @staticmethod
+    def serialize(root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        stack = deque([root]) if root else deque()
+        res = []
+        while stack:
+            node = stack.popleft()
+            if not node:
+                res.append("n")
+                continue
+            else:
+                res.append(str(node.val))
+                stack.append(node.left)
+                stack.append(node.right)
+        return ",".join(res)
+
+    @staticmethod
+    def deserialize(data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        if not data:
+            return
+        lst = deque(data.split(","))
+        ans = TreeNode(int(lst.popleft()))
+        stack = deque([ans])
+        while lst:
+            left, right = lst.popleft(), lst.popleft()
+            pre = stack.popleft()
+            if left != "n":
+                pre.left = TreeNode(int(left))
+                stack.append(pre.left)
+            if right != "n":
+                pre.right = TreeNode(int(right))
+                stack.append(pre.right)
+        return ans
+
+
+class CodecDFS:
+
+    @staticmethod
+    def serialize(root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        """
+        def dfs(node):
+            if not node:
+                return "n"
+            return dfs(node.right) + "," + dfs(node.left) + "," + str(node.val)
+        return dfs(root)
+
+    @staticmethod
+    def deserialize(data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        """
+        lst = data.split(",")
+
+        def dfs():
+            if not lst:
+                return
+            val = lst.pop()
+            if val == "n":
+                return
+            root = TreeNode(int(val))
+            root.left = dfs()
+            root.right = dfs()
+            return root
+        return dfs()
 
 
 class Solution:
