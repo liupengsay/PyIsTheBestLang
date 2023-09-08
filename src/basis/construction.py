ee7cebab556f73a1feda3da3f05ce23d22a56079
liupengsay <liupengsay@foxmail.com>
2023-09-08 21:45:46 +0800

    经典构造贪心思维

--- src/basis/construction.py
+++ src/basis/construction.py
@@ -17,6 +17,7 @@ from src.mathmatics.number_theory import NumberTheory
 280. 摆动排序（https://leetcode.cn/problems/wiggle-sort/）按照降序先赋给奇数索引再给偶数索引
 2663. 字典序最小的美丽字符串（https://leetcode.cn/problems/lexicographically-smallest-beautiful-string/）贪心构造不含任何回文子串的字典序最小的字符串
 1982. 从子集的和还原数组（https://leetcode.cn/problems/find-array-given-subset-sums/）经典构造
+1253. 重构 2 行二进制矩阵（https://leetcode.cn/problems/reconstruct-a-2-row-binary-matrix/）经典构造贪心思维
 
 ===================================洛谷===================================
 P8846 『JROI-7』PMK 配匹串符字（https://www.luogu.com.cn/problem/P8846）厘清题意进行贪心构造
