2bf6c855d2f1f74f8ce5f67e59122799fe6943a8
liupengsay <liupengsay@foxmail.com>
2023-09-12 22:14:51 +0800

    使用KMP与快速幂进行转移计算，也可使用字符串哈希

--- src/string/string_hash.py
+++ src/string/string_hash.py
@@ -8,6 +8,7 @@ import random
 
 from src.basis.binary_search import BinarySearch
 from src.fast_io import FastIO
+from src.mathmatics.fast_power import MatrixFastPower
 
 """
 
@@ -26,6 +27,7 @@ from src.fast_io import FastIO
 1923. 最长公共子路径（https://leetcode.cn/problems/longest-common-subpath/）经典二分查找加滚动哈希
 1948. 删除系统中的重复文件夹（https://leetcode.cn/problems/delete-duplicate-folders-in-system/）字典树与树哈希去重
 2261. 含最多 K 个可整除元素的子数组（https://leetcode.cn/problems/k-divisible-elements-subarrays/submissions/）使用字符串哈希对数组进行编码
+2851. 字符串转换（https://leetcode.cn/problems/string-transformation/description/）使用KMP与快速幂进行转移计算，也可使用字符串哈希
 
 ===================================洛谷===================================
 P8835 [传智杯 #3 决赛] 子串（https://www.luogu.com.cn/record/list?user=739032&status=12&page=14）字符串哈希或者KMP查找匹配的连续子串
@@ -388,6 +390,20 @@ class Solution:
                 ac.st("different")
         return
 
+    @staticmethod
+    def lc_2851(s: str, t: str, k: int) -> int:
+        # 模板：使用KMP与快速幂进行转移计算，也可使用字符串哈希（超时）
+        mod = 10**9 + 7
+        n = len(s)
+        sh = StringHash(3 * n + 1, t + "#" + s + s)
+        target = sh.query(0, n - 1)
+        p = sum(sh.query(i - n + 1, i) == target for i in range(2 * n, 3 * n))
+        q = n - p
+        mat = [[p - 1, p], [q, q - 1]]
+        vec = [1, 0] if sh.query(n + 1, 2 * n) == target else [0, 1]
+        res = MatrixFastPower().matrix_pow(mat, k, mod)
+        ans = vec[0] * res[0][0] + vec[1] * res[0][1]
+        return ans % mod
     @staticmethod
     def lg_p2852(ac=FastIO()):
 
