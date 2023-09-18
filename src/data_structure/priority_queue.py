e29df70c63dbfe9ef0ae9e3581e8028062f4e765
liupengsay <liupengsay@foxmail.com>
2023-09-18 23:44:59 +0800

    单调队列DP模板题

--- src/data_structure/priority_queue.py
+++ src/data_structure/priority_queue.py
@@ -15,6 +15,7 @@ from src.fast_io import FastIO
 239. 滑动窗口最大值（https://leetcode.cn/problems/sliding-window-maximum/）滑动区间最大值
 1696. 跳跃游戏 VI（https://leetcode.cn/problems/jump-game-vi/）经典优先队列 DP
 862. 和至少为 K 的最短子数组（https://leetcode.cn/problems/shortest-subarray-with-sum-at-least-k/description/）前缀和加单调双端队列DP
+1425. 带限制的子序列和（https://leetcode.cn/problems/constrained-subsequence-sum/）单调队列DP模板题
 
 ===================================洛谷===================================
 P2251 质量检测（https://www.luogu.com.cn/problem/P2251）滑动区间最小值
