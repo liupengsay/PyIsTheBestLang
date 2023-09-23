85ebe821db728daffc0fce403350613f7ef17266
liupengsay <liupengsay@foxmail.com>
2023-09-24 00:48:07 +0800

    通过位运算枚举分配工作DP最小化的最大值，枚举子集预处理

--- src/dp/state_dp.py
+++ src/dp/state_dp.py
@@ -20,7 +20,7 @@ from src.fast_io import FastIO
 ===================================力扣===================================
 465. 最优账单平衡（https://leetcode.cn/problems/optimal-account-balancing/）经典枚举子集状压DP
 1349. 参加考试的最大学生数（https://leetcode.cn/problems/maximum-students-taking-exam/）按行状态枚举所有的摆放可能性
-1723. 完成所有工作的最短时间（https://leetcode.cn/problems/find-minimum-time-to-finish-all-jobs/）通过位运算枚举分配工作DP最小化的最大值
+1723. 完成所有工作的最短时间（https://leetcode.cn/problems/find-minimum-time-to-finish-all-jobs/）通过位运算枚举分配工作DP最小化的最大值，枚举子集预处理
 1986. 完成任务的最少工作时间段（https://leetcode.cn/problems/minimum-number-of-work-sessions-to-finish-the-tasks/）预处理计算子集后进行记忆化状态转移，经典子集枚举，也可使用两个状态
 698. 划分为k个相等的子集（https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/）预处理计算子集后进行记忆化状态转移
 2172. 数组的最大与和（https://leetcode.cn/problems/maximum-and-sum-of-array/）使用位运算和状态压缩进行转移，经典三进制状压DP（天平就是三进制）
@@ -110,6 +110,37 @@ class Solution:
         m = n // k
         ans = dfs((1 << n) - 1)
         return ans if ans < inf else -1
+
+    @staticmethod
+    def lc_1723(jobs: List[int], k: int) -> int:
+        # 模板：通过位运算枚举分配工作DP最小化的最大值，枚举子集预处理
+
+        @lru_cache(None)
+        def dfs(i, state):
+            if i == k:
+                if not state:
+                    return 0
+                return inf
+            res = inf
+            sub = state
+            while sub:
+                cost = state_cost[sub]
+                if cost < res:
+                    nex = dfs(i + 1, state ^ sub)
+                    if nex > cost:
+                        cost = nex
+                    if cost < res:
+                        res = cost
+                # 枚举子集模板
+                sub = (sub - 1) & state
+            return res
+
+        n = len(jobs)
+        state_cost = [0] * (1 << n)
+        for x in range(1, 1 << n):
+            state_cost[x] = sum(jobs[j] for j in range(n) if x & (1 << j))
+
+        return dfs(0, (1 << n) - 1)
 
     @staticmethod
     def lc_1879_1(nums1: List[int], nums2: List[int]) -> int:
@@ -607,7 +638,6 @@ class Solution:
             return min(dfs(i + 1, state | (1 << j)) + cost[i][j] for j in range(n))
 
         return dfs(0, 0)
-
 
     @staticmethod
     def lc_1655(nums: List[int], quantity: List[int]) -> bool:
