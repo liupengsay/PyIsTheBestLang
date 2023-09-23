4f1ee2498c8da9c660bab33857f80d434cf63abf
liupengsay <liupengsay@foxmail.com>
2023-09-24 00:47:59 +0800

    dfs序模板题目

--- src/search/dfs.py
+++ src/search/dfs.py
@@ -36,6 +36,7 @@ from src.graph.lca import TreeAncestor
 1080. 根到叶路径上的不足节点（https://leetcode.cn/problems/insufficient-nodes-in-root-to-leaf-paths/description/）经典dfs自上而下后又自下而上
 2056. 棋盘上有效移动组合的数目（https://leetcode.cn/problems/number-of-valid-move-combinations-on-chessboard/description/）经典回溯枚举
 100041. 可以到达每一个节点的最少边反转次数（https://www.acwing.com/problem/content/description/4384/）迭代法实现树形换根DP计算，或者一遍DFS或者dfs序加差分
+2458. 移除子树后的二叉树高度（https://leetcode.cn/contest/weekly-contest-317/problems/height-of-binary-tree-after-subtree-removal-queries/）dfs序模板题目
 
 ===================================洛谷===================================
 P2383 狗哥玩木棒（https://www.luogu.com.cn/problem/P2383）暴力搜索木棍拼接组成正方形
@@ -84,7 +85,7 @@ class DFS:
         return
 
     @staticmethod
-    def gen_bfs_order_iteration(dct):
+    def gen_bfs_order_iteration(dct, root=0):
         # 模板：生成深搜序即 dfs 序以及对应子树编号区间
         n = len(dct)
         for i in range(n):
@@ -93,7 +94,7 @@ class DFS:
         start = [-1] * n  # 每个原始节点的dfs序号开始点也是node_to_order
         end = [-1]*n  # 每个原始节点的dfs序号结束点
         parent = [-1]*n  # 每个原始节点的父节点
-        stack = [[0, -1]]
+        stack = [[root, -1]]
         depth = [0]*n  # 每个原始节点的深度
         order_to_node = [-1]*n  # 每个dfs序号对应的原始节点编号
         while stack:
@@ -118,23 +119,24 @@ class DFS:
 
 
 class DfsEulerOrder:
-    def __init__(self, dct):
+    def __init__(self, dct, root=0):
         # 模板：dfs序与欧拉序，支持在线区间修改树上边，并且实时查询任意两点树上距离
         n = len(dct)
         for i in range(n):
             dct[i].sort(reverse=True)  # 按照子节点编号从小到大进行遍历
         self.start = [-1] * n  # 每个原始节点的dfs序号开始点也是node_to_order
-        self.end = [-1]*n  # 每个原始节点的dfs序号结束点
-        self.parent = [-1]*n  # 每个原始节点的父节点
-        self.depth = [0]*n  # 每个原始节点的深度
-        self.order_to_node = [-1]*n  # 每个dfs序号对应的原始节点编号
+        self.end = [-1] * n  # 每个原始节点的dfs序号结束点
+        self.parent = [-1] * n  # 每个原始节点的父节点
+        self.order_to_node = [-1] * n  # 每个dfs序号对应的原始节点编号
+        self.node_depth = [0] * n  # 每个原始节点的深度
+        self.order_depth = [0] * n  # 每个dfs序号的深度
         self.euler_order = []  # 每个dfs序回溯得到的欧拉序号的原始节点编号
-        self.euler_in = [-1]*n  # 每个原始节点再欧拉序中首次出现的位置
-        self.euler_out = [-1]*n   # 每个原始节点再欧拉序中最后出现的位置
-        self.build(dct)
+        self.euler_in = [-1] * n  # 每个原始节点再欧拉序中首次出现的位置
+        self.euler_out = [-1] * n  # 每个原始节点再欧拉序中最后出现的位置
+        self.build(dct, root)
         return
 
-    def build(self, dct, root=0):
+    def build(self, dct, root):
         # 生成dfs序与欧拉序相关信息
         order = 0
         stack = [[root, -1]]
@@ -145,12 +147,14 @@ class DfsEulerOrder:
                 self.start[i] = order
                 self.order_to_node[order] = i
                 self.end[i] = order
+                self.order_depth[order] = self.node_depth[i]
                 order += 1
                 stack.append([~i, fa])
                 for j in dct[i]:
                     if j != fa:  # 注意访问顺序可以进行调整，比如字典序正序逆序
                         self.parent[j] = i
-                        self.depth[j] = self.depth[i] + 1
+                        self.node_depth[j] = self.node_depth[i] + 1
+
                         stack.append([j, i])
             else:
                 i = ~i
@@ -162,18 +166,6 @@ class DfsEulerOrder:
             self.euler_out[num] = i  # 计算欧拉序的位置
             if self.euler_in[num] == -1:
                 self.euler_in[num] = i
-        return
-
-    def query_dis(self, u, v):
-        # 利用欧拉序计算树上任意两点的距离
-        return
-
-    def query_lca(self, u, v):
-        # 利用欧拉序计算树上任意两点的最近公共祖先
-        return
-
-    def update_edge(self, u, v):
-        # 利用欧拉序更新树上边的距离
         return
 
 
@@ -533,6 +525,43 @@ class Solution:
                 cur_ = max(cur) - min(cur)
                 if cur_ < ans:
                     ans = cur_
+        return ans
+
+    @staticmethod
+    def lc_2458(root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        # 模板：dfs序模板题目
+
+        def dfs(node):
+            nonlocal n
+            if not node:
+                return
+            x = node.val - 1
+            if x + 1 > n:
+                n = x + 1
+            if node.left:
+                dct[x].append(node.left.val - 1)
+                dfs(node.left)
+            if node.right:
+                dct[x].append(node.right.val - 1)
+                dfs(node.right)
+            return
+
+        dct = defaultdict(list)
+        n = 0
+        dfs(root)
+        edge = [dct[i] for i in range(n)]
+        r = root.val - 1
+        dfs_order = DfsEulerOrder(edge, r)
+        pre = [0] + list(accumulate(dfs_order.order_depth[:], func=max))
+        post = list(accumulate(dfs_order.order_depth[::-1], func=max))[::-1] + [0]
+
+        ans = []
+        for i in queries:
+            i -= 1
+            s, e = dfs_order.start[i], dfs_order.end[i]
+            a = pre[s]
+            b = post[e + 1]
+            ans.append(a if a > b else b)
         return ans
 
     @staticmethod
