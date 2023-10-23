cec4c2c90c69143afd2c8795f02bca75da6a7fb4
liupengsay <liupengsay@foxmail.com>
2023-10-23 08:34:28 +0800

    english comments

--- source/data_structure/tree_array/template.py
+++ source/data_structure/tree_array/template.py
@@ -24,7 +24,7 @@ class PointAddRangeSum:
         return
 
     def point_add(self, i: int, mi: int) -> None:
-        # index start from 1 and the value mi can be any inter including positive and negtive number
+        # index start from 1 and the value mi can be any inter including positive and negative number
         assert 1 <= i <= self.n
         while i < len(self.t):
             self.t[i] += mi
@@ -39,7 +39,7 @@ class PointAddRangeSum:
         return nums
 
     def _pre_sum(self, i: int) -> int:
-        # index start from 1 and the prefix sum of nums[1:i+1]
+        # index start from 1 and the prefix sum of nums[:i] which is 0-index
         assert 1 <= i <= self.n
         mi = 0
         while i:
@@ -48,7 +48,7 @@ class PointAddRangeSum:
         return mi
 
     def range_sum(self, x: int, y: int) -> int:
-        # index start from 1 and the range sum of nums[x:y+1]
+        # index start from 1 and the range sum of nums[x-1:y]  which is 0-index
         assert 1 <= x <= y <= self.n
         res = self._pre_sum(y) - self._pre_sum(x - 1) if x > 1 else self._pre_sum(y)
         return res
@@ -62,7 +62,7 @@ class PointAddRangeSum2D:
         return
 
     def point_add(self, x: int, y: int, val: int) -> None:
-        # 索引从 1 开始， 单点增加 val 到二维数组中坐标为 [x, y] 的值且 val 可正可负
+        # index start from 1 and val can be any integer
         i = x
         while i <= self.m:
             j = y
@@ -73,7 +73,7 @@ class PointAddRangeSum2D:
         return
 
     def _query(self, x: int, y: int) -> int:
-        # 索引从 1 开始， 查询二维数组中 [1, 1] 到 [x, y] 的前缀和
+        # index start from 1 and query the sum of prefix matrix sum(s[:y] for s in grid[:x])  which is 0-index
         res = 0
         i = x
         while i > 0:
@@ -85,7 +85,7 @@ class PointAddRangeSum2D:
         return res
 
     def range_sum(self, x1: int, y1: int, x2: int, y2: int) -> int:
-        # 索引从 1 开始， 查询二维数组中 [x1, y1] 到 [x2, y2] 的区间和
+        # index start from 1 and query the sum of matrix sum(s[y1-1:y2] for s in grid[x1-1: x2])  which is 0-index
         return self._query(x2, y2) - self._query(x2, y1 - 1) - self._query(x1 - 1, y2) + self._query(x1 - 1, y1 - 1)
 
 
@@ -93,25 +93,21 @@ class RangeAddRangeSum:
 
     def __init__(self, n: int) -> None:
         self.n = n
-        # 索引从 1 开始
         self.t1 = [0] * (n + 1)
         self.t2 = [0] * (n + 1)
         return
 
     @staticmethod
     def _lowest_bit(x: int) -> int:
-        # 经典 _lowest_bit 即最后一位二进制为 1 所表示的数
         return x & (-x)
 
     def build(self, nums: List[int]) -> None:
-        # 索引从 1 开始使用数组初始化树状数组
         assert len(nums) == self.n
         for i in range(self.n):
             self.range_add(i + 1, i + 1, nums[i])
         return
 
     def get(self) -> List[int]:
-        # 索引从 1 开始使用数组初始化树状数组
         nums = [0] * self.n
         for i in range(self.n):
             nums[i] = self.range_sum(i + 1, i + 1)
@@ -119,7 +115,7 @@ class RangeAddRangeSum:
 
     # 更新单点的差分数值
     def _add(self, k: int, v: int) -> None:
-        # 索引从 1 开始将第 k 个数加 v 且 v 可正可负
+        # start from index 1 and v can be any integer
         v1 = k * v
         while k <= self.n:
             self.t1[k] = self.t1[k] + v
@@ -127,46 +123,39 @@ class RangeAddRangeSum:
             k = k + self._lowest_bit(k)
         return
 
-    # 求差分数组的前缀和
     def _sum(self, t: List[int], k: int) -> int:
-        # 索引从 1 开始求前 k 个数的前缀和
+        # index start from 1 and query the sum of prefix k number
         ret = 0
         while k:
             ret = ret + t[k]
             k = k - self._lowest_bit(k)
         return ret
 
-    # 更新差分的区间数值
     def range_add(self, left: int, right: int, v: int) -> None:
-        # 索引从 1 开始将区间 [left, right] 的数增加 v 且 v 可正可负
+        # index start from 1 and v van be any integer
         self._add(left, v)
         self._add(right + 1, -v)
         return
 
     # 求数组的前缀区间和
     def range_sum(self, left: int, right: int) -> int:
-        # 索引从 1 开始查询区间 [left, right] 的和
+        # index start from 1 and query the sum(nums[left-1: right]) which is 0-index array
         a = (right + 1) * self._sum(self.t1, right) - self._sum(self.t2, right)
         b = left * self._sum(self.t1, left - 1) - self._sum(self.t2, left - 1)
         return a - b
 
 
 class PointXorRangeXor:
-    # 模板：树状数组 单点异或 查询前缀异或和与区间异或和
     def __init__(self, n: int) -> None:
-        # 索引从 1 到 n
         self.n = n
         self.t = [0] * (n + 1)
-        # 树状数组中每个位置保存的是其向前 _lowest_bit 的区间异或和
         return
 
     @staticmethod
     def _lowest_bit(i: int) -> int:
-        # 经典 _lowest_bit 即最后一位二进制为 1 所表示的数
         return i & (-i)
 
     def _pre_xor(self, i: int) -> int:
-        # 索引从 1 开始，查询 1 到 i 的前缀区间和
         assert 1 <= i <= self.n
         mi = 0
         while i:
@@ -175,7 +164,6 @@ class PointXorRangeXor:
         return mi
 
     def build(self, nums: List[int]) -> None:
-        # 索引从 1 开始使用数组初始化树状数组
         assert len(nums) == self.n
         pre = [0] * (self.n + 1)
         for i in range(self.n):
@@ -184,11 +172,9 @@ class PointXorRangeXor:
         return
 
     def get(self) -> List[int]:
-        # 索引从 1 开始使用数组初始化树状数组
         return [self.range_xor(i + 1, i + 1) for i in range(self.n)]
 
     def point_xor(self, i: int, mi: int) -> None:
-        # 索引从 1 开始，索引 i 的值异或增加 mi 且 mi 可正可负
         assert 1 <= i <= self.n
         while i <= self.n:
             self.t[i] ^= mi
@@ -196,16 +182,13 @@ class PointXorRangeXor:
         return
 
     def range_xor(self, x: int, y: int) -> int:
-        # 索引从 1 开始，查询 x 到 y 的值
         assert 1 <= x <= y <= self.n
         res = self._pre_xor(y) ^ self._pre_xor(x - 1) if x > 1 else self._pre_xor(y)
         return res
 
 
 class PointAscendPreMax:
-    # 模板：树状数组 单点增加 前缀区间查询 最大值
     def __init__(self, n, initial=-inf):
-        # 索引从 1 到 n
         self.n = n
         self.initial = initial
         self.t = [initial] * (n + 1)
@@ -231,7 +214,6 @@ class PointAscendPreMax:
 
 
 class PointAscendRangeMax:
-    # 模板：树状数组 单点持续增加 区间查询最大值
     def __init__(self, n: int, initial=-inf) -> None:
         self.n = n
         self.initial = initial
@@ -248,7 +230,6 @@ class PointAscendRangeMax:
         return a if a > b else b
 
     def point_ascend(self, x, k):
-        # 索引从1开始，单点更新最大值
         assert 1 <= x <= self.n
         if self.a[x] >= k:
             return
@@ -259,7 +240,7 @@ class PointAscendRangeMax:
         return
 
     def range_max(self, left, r):
-        # 索引从1开始
+        assert 1 <= left <= r <= self.n
         max_val = self.initial
         while r >= left:
             if r - self._lowest_bit(r) >= left - 1:
@@ -272,9 +253,7 @@ class PointAscendRangeMax:
 
 
 class PointDescendPreMin:
-    # 模板：树状数组 单点减少 前缀区间查询最小值
     def __init__(self, n, initial=inf):
-        # 索引从 1 到 n
         self.n = n
         self.initial = initial
         self.t = [self.initial] * (n + 1)
@@ -300,8 +279,6 @@ class PointDescendPreMin:
 
 
 class PointDescendRangeMin:
-
-    # 模板：树状数组 单点持续减少 区间查询最小值
     def __init__(self, n: int, initial=inf) -> None:
         self.n = n
         self.initial = initial
@@ -318,7 +295,6 @@ class PointDescendRangeMin:
         return a if a < b else b
 
     def point_descend(self, x, k):
-        # 索引从1开始，单点更新最小值
         assert 1 <= x <= self.n
         if self.a[x] <= k:
             return
@@ -329,7 +305,6 @@ class PointDescendRangeMin:
         return
 
     def range_min(self, left, r):
-        # 索引从1开始
         assert 1 <= left <= r <= self.n
         min_val = self.initial
         while r >= left:
@@ -344,19 +319,18 @@ class PointDescendRangeMin:
 
 class RangeAddRangeSum2D:
     def __init__(self, m: int, n: int) -> None:
-        # 模板：二维树状数组 区间增减 区间和查询
-        self.m = m  # 行数
-        self.n = n  # 列数
+        self.m = m  # row
+        self.n = n  # col
         self.m = m
         self.n = n
-        self.t1 = [[0] * (n + 1) for _ in range(m + 1)]  # 初始化树状数组
-        self.t2 = [[0] * (n + 1) for _ in range(m + 1)]  # 初始化树状数组
-        self.t3 = [[0] * (n + 1) for _ in range(m + 1)]  # 初始化树状数组
-        self.t4 = [[0] * (n + 1) for _ in range(m + 1)]  # 初始化树状数组
+        self.t1 = [[0] * (n + 1) for _ in range(m + 1)]
+        self.t2 = [[0] * (n + 1) for _ in range(m + 1)]
+        self.t3 = [[0] * (n + 1) for _ in range(m + 1)]
+        self.t4 = [[0] * (n + 1) for _ in range(m + 1)]
         return
 
     def _add(self, x: int, y: int, val: int) -> None:
-        # 索引从 1 开始， 单点增加 val 到二维数组中坐标为 [x, y] 的差分数组值且 val 可正可负
+        # index start from 1 and single point add val and val cam be any integer
         i = x
         while i <= self.m:
             j = y
@@ -370,7 +344,7 @@ class RangeAddRangeSum2D:
         return
 
     def range_add(self, x1: int, y1: int, x2: int, y2: int, val: int) -> None:
-        # 索引从 1 开始， 区间增加 val 到二维数组中坐标为左上角 [x1, y1] 到右下角的 [x2, y2] 且 val 可正可负
+        # index start from 1 and left up corner is (x1, y1) and right down corner is (x2, y2) and val can be any integer
         self._add(x1, y1, val)
         self._add(x1, y2 + 1, -val)
         self._add(x2 + 1, y1, -val)
@@ -378,7 +352,8 @@ class RangeAddRangeSum2D:
         return
 
     def _query(self, x: int, y: int) -> int:
-        # 索引从 1 开始， 查询二维数组中 [1, 1] 到 [x, y] 的前缀和
+        # index start from 1 and query the sum(sum(g[:y]) for g in grid[:x]) which is 0-index
+        assert 1 <= x <= self.m and 1 <= y <= self.n
         res = 0
         i = x
         while i:
@@ -391,18 +366,18 @@ class RangeAddRangeSum2D:
         return res
 
     def range_query(self, x1: int, y1: int, x2: int, y2: int) -> int:
-        # 索引从 1 开始， 查询二维数组中 [x1, y1] 到 [x2, y2] 的区间和
+        # index start from 1 and left up corner is (x1, y1) and right down corner is (x2, y2)
         return self._query(x2, y2) - self._query(x2, y1 - 1) - self._query(x1 - 1, y2) + self._query(x1 - 1, y1 - 1)
 
 
 class PointChangeMaxMin2D:
-    # 模板：树状数组 单点增加区间查询最大值 单点减少区间查询最小值（暂未调通）
+    # not already for use and there still exist some bug
     def __init__(self, m: int, n: int) -> None:
         self.m = m
         self.n = n
         self.a = [[0] * (n + 1) for _ in range(m + 1)]
-        self.tree_ceil = [[0] * (n + 1) for _ in range(m + 1)]  # 最大值只能持续增加
-        self.tree_floor = [[float('inf')] * (n + 1) for _ in range(m + 1)]  # 最小值只能持续减少
+        self.tree_ceil = [[0] * (n + 1) for _ in range(m + 1)]  # point keep ascend
+        self.tree_floor = [[float('inf')] * (n + 1) for _ in range(m + 1)]  # point keep descend
         return
 
     @staticmethod
@@ -418,7 +393,7 @@ class PointChangeMaxMin2D:
         return a if a < b else b
 
     def add(self, x, y, k):
-        # 索引从1开始
+        # index start from 1
         self.a[x][y] = k
         i = x
         while i <= self.m:
@@ -431,8 +406,8 @@ class PointChangeMaxMin2D:
         return
 
     def find_max(self, x1, y1, x2, y2):
-        # 索引从1开始
-        max_val = float('-inf')
+        assert 1 <= x1 <= x2 <= self.m and 1 <= y1 <= y2 <= self.n
+        max_val = inf
         i1, i2 = x1, x2
         while i2 >= i1:
             if i2 - self._lowest_bit(i2) >= i1 - 1:
