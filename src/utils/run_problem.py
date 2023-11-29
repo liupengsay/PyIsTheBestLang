43498b6f10db9316702322e96b4a75e96b845982
liupengsay <liupengsay@foxmail.com>
2023-11-29 08:08:47 +0800

    经典建图后使用染色法BFS判断二分图

---
+++ src/utils/run_problem.py
@@ -0,0 +1,34 @@
+import os
+import random
+import time
+
+
+def process_file(file_path):
+    with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
+        lines = file.readlines()
+    filtered_lines = [line.strip("\n") for line in lines if "https" in line]
+    tot.extend(filtered_lines)
+    return
+
+
+def process_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == "problem.py":
+                file_path = os.path.join(root, file)
+                process_file(file_path)
+    return
+
+
+current_path = os.getcwd()  # 获取当前路径
+parent_path = os.path.abspath(os.path.join(current_path, os.pardir))  # 获取父级路径
+grandparent_path = os.path.abspath(os.path.join(parent_path, os.pardir))  # 获取上上级路径
+# 示例用法
+s = time.time()
+tot = []
+process_directory(os.path.join(grandparent_path, "src"))
+random.shuffle(tot)
+with open(os.path.join(grandparent_path, "data/problem.txt"), 'w', encoding="utf-8", errors="ignore") as f:
+    f.writelines("\n".join(tot))
+
+print("总耗时: ", int(time.time() - s), "s")
