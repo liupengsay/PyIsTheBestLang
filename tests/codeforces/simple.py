8a4d1f76086df2d4389bc5f95c49d84ddcd9293e
liupengsay <liupengsay@foxmail.com>
2023-12-14 07:51:16 +0800

    pass

--- tests/codeforces/simple.py
+++ tests/codeforces/simple.py
@@ -86,11 +86,8 @@ class Solution:
     @staticmethod
     def main(ac=FastIO()):
 
-        def solve():
-            return
-
         for _ in range(ac.read_int()):
-            solve()
+            pass
         return
 
 
--- tests/codeforces/template.py
+++ tests/codeforces/template.py
@@ -143,12 +143,9 @@ class Solution:
 
     @staticmethod
     def main(ac=FastIO()):
-        def solve():
-
-            return
 
         for _ in range(ac.read_int()):
-            solve()
+            pass
         return
 
 
