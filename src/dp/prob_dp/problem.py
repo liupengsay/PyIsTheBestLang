64d41710fd6c661fd509f2e236972cdda4bf53ca
liupengsay <liupengsay@foxmail.com>
2023-12-29 14:33:47 +0800

    review

--- src/dp/prob_dp/problem.py
+++ src/dp/prob_dp/problem.py
@@ -6,7 +6,6 @@ Description：comb|specific_plan|prob|expectation
 1227（https://leetcode.cn/problems/airplane-seat-assignment-probability/）prob_dp
 
 =====================================LuoGu======================================
-list?user=739032&status=12&page=1（https://www.luogu.com.cn/record/list?user=739032&status=12&page=1）matrix_dp|prob
 P1291（https://www.luogu.com.cn/problem/P1291）liner_dp|expectation
 P4316（https://www.luogu.com.cn/problem/P4316）expectation|reverse_graph|topological_sort
 P6154（https://www.luogu.com.cn/problem/P6154）reverse_graph|expectation|tree_dp|float|mod
@@ -18,7 +17,6 @@ P6154（https://www.luogu.com.cn/problem/P6154）reverse_graph|expectation|tree_
 """
 import math
 from collections import deque
-from functools import lru_cache
 
 from src.utils.fast_io import FastIO
 
@@ -28,49 +26,22 @@ class Solution:
         return
 
     @staticmethod
-    def main(ac=FastIO()):
-
-        # memory_search二维 DP implemention搜索转移概率
-
-        @lru_cache(None)
-        def dfs(a, b):
-            if a + b == 2:
-                if a == 0 or b == 0:
-                    return 1
-                return 0
-            if a == 0 or b == 0:
-                return 1
-            res = (dfs(a - 1, b) + dfs(a, b - 1)) / 2
-            return res
-
-        n = ac.read_int() // 2
-        if n == 0:
-            ans = 0
-        else:
-            ans = dfs(n, n)
-        ac.st("%.4f" % ans)
-        return
-
-    @staticmethod
     def lg_p1291(ac=FastIO()):
         """
         url: https://www.luogu.com.cn/problem/P1291
         tag: liner_dp|expectation
         """
 
-        # liner_dp求expectation，分数|减运算
         n = ac.read_int()
         ans = [1, 1]
         for x in range(2, n + 1):
             a, b = ans
             c, d = 1, x
-            # gcd|减
             g = math.gcd(b, d)
             lcm = b * d // g
             a, b = a * lcm // b + c * lcm // d, lcm
             g = math.gcd(a, b)
             ans = [a // g, b // g]
-        # f[i] = f[i-1] + n/(n-i+1) 表示已经有 i-1 个再要有新的一个的expectation为 n/(n-i+1)
         a, b = ans
         a *= n
         x = a // b
@@ -78,7 +49,6 @@ class Solution:
         if a == 0:
             ac.st(x)
             return
-        # |和化简
         g = math.gcd(a, b)
         ans = [a // g, b // g]
         a, b = ans
@@ -91,9 +61,8 @@ class Solution:
     def lg_p4316(ac=FastIO()):
         """
         url: https://www.luogu.com.cn/problem/P4316
-        tag: expectation|reverse_graph|topological_sort
+        tag: expectation|reverse_graph|topological_sort|float
         """
-        # expectation DP reverse_graph与topological_sorting
         n, m = ac.read_list_ints()
         dp = [0 for _ in range(n)]
         degree = [0] * n
@@ -106,7 +75,6 @@ class Solution:
             degree[a] += 1
         cnt = degree[:]
 
-        # 反向topological_sorting与状态转移
         stack = deque([n - 1])
         while stack:
             i = stack.popleft()
@@ -126,7 +94,7 @@ class Solution:
         url: https://www.luogu.com.cn/problem/P6154
         tag: reverse_graph|expectation|tree_dp|float|mod
         """
-        # reverse_graphexpectationtree_dp| 与有理数mod|
+
         n, m = ac.read_list_ints()
         dct = [[] for _ in range(n)]
         degree = [0] * n
@@ -134,9 +102,7 @@ class Solution:
             x, y = ac.read_list_ints_minus_one()
             dct[y].append(x)
             degree[x] += 1
-        # 当前节点为起点的路径总长度
         length_sum = [0] * n
-        # 当前节点为起点的路径总数
         path_cnt = [0] * n
         mod = 998244353
         stack = deque([i for i in range(n) if not degree[i]])
@@ -146,12 +112,9 @@ class Solution:
             i = stack.popleft()
             for j in dct[i]:
                 degree[j] -= 1
-                # 路径总长度增|
                 length_sum[j] += path_cnt[i] + length_sum[i]
-                # 路径条数增|
                 path_cnt[j] += path_cnt[i]
                 if not degree[j]:
-                    # 新增一条以当前点开始和结束的路径即起点与终点可以相同
                     path_cnt[j] += 1
                     path_cnt[j] %= mod
                     length_sum[j] %= mod
@@ -167,7 +130,6 @@ class Solution:
         url: https://www.acwing.com/problem/content/description/5061/
         tag: prob_dp
         """
-        # prob_dp
         w, b = ac.read_list_ints()
         dp = [[0] * (b + 1) for _ in range(w + 1)]
         for i in range(1, w + 1):
