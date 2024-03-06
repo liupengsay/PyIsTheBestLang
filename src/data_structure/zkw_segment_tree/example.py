e612af7fc2b4a88fadffd7594f377e906817ec50
liupengsay <liupengsay@foxmail.com>
2024-03-06 20:23:41 +0800

    LazySegmentTree

--- src/data_structure/zkw_segment_tree/example.py
+++ src/data_structure/zkw_segment_tree/example.py
@@ -8,7 +8,7 @@ from src.data_structure.tree_array.template import PointAddRangeSum as PointAddR
 from src.data_structure.zkw_segment_tree.template import (
     PointSetAddRangeSum as PointSetRangeSumZKW,
     RangeAddPointGet as RangeAddPointGetZKW,
-    LazySegmentTree as LazySegmentTreeZKW)
+    LazySegmentTree as LazySegmentTreeZKW, LazySegmentTreeLength)
 
 
 class TestGeneral(unittest.TestCase):
@@ -270,6 +270,120 @@ class TestGeneral(unittest.TestCase):
             assert ans == sum(nums[ll:rr + 1])
 
         assert nums == tree.get()
+        return
+
+    def test_range_set_reverse_range_sum_longest_con_sub_sum(self):
+        random.seed(2024)
+        high = 5 * 100000
+
+        # start, end, cover_0, cover_1, length, sum
+        # start>0 1 start<0 0
+        # 0-change 1-change 2-reverse 3-null
+        nums = [random.randint(0, 1) for _ in range(high)]
+
+        def merge_cover_tag(cover, tag):
+            length = cover[-2]
+            if tag == 0:
+                return -length, -length, length, 0, length, 0
+            elif tag == 1:
+                return length, length, 0, length, length, length
+            elif tag == 2:  # 2
+                start, end, cover_zero, cover_one, length, tot = cover
+                return -start, -end, cover_one, cover_zero, length, length - tot
+            return cover
+
+        def combine_cover_cover(cover1, cover2):
+            start1, end1, cover_zero1, cover_one1, length1, sum1 = cover1
+            if cover1 == cover_initial:
+                return cover2
+            if cover2 == cover_initial:
+                return cover1
+            start2, end2, cover_zero2, cover_one2, length2, sum2 = cover2
+            start = start1
+            if (start1 == length1 or start1 == -length1) and start1 * start2 > 0:
+                start += start2
+            end = end2
+            if (start2 == length2 or start2 == -length2) and end1 * end2 > 0:
+                end += end1
+
+            cover_zero = cover_zero1 if cover_zero1 > cover_zero2 else cover_zero2
+            if start < 0 and -start > cover_zero:
+                cover_zero = -start
+            if end < 0 and -end > cover_zero:
+                cover_zero = -end
+
+            cover_one = cover_one1 if cover_one1 > cover_one2 else cover_one2
+            if start > 0 and start > cover_one:
+                cover_one = start
+            if end > 0 and end > cover_one:
+                cover_one = end
+
+            if end1 > 0 and start2 > 0 and end1 + start2 > cover_one:
+                cover_one = end1 + start2
+            if end1 < 0 and start2 < 0 and -end1 - start2 > cover_zero:
+                cover_zero = -end1 - start2
+
+            length = length1 + length2
+            return start, end, cover_zero, cover_one, length, sum1 + sum2
+
+        def merge_tag_tag(tag1, tag2):
+            if tag1 == 3:
+                return tag2
+            if tag1 <= 1:
+                return tag1
+            # tag1 = 2
+            if tag2 <= 1:
+                tag2 = 1 - tag2
+            elif tag2 == 2:
+                tag2 = 3
+            else:
+                tag2 = 2
+            return tag2
+
+        cover_initial = (0, 0, 0, 0, 0, 0)
+        tag_initial = 3
+
+        tree = LazySegmentTreeLength(high, combine_cover_cover, cover_initial, merge_cover_tag, merge_tag_tag,
+                                     tag_initial)
+        tree.build(nums)
+
+        def check(tmp):
+            ans = pre = 0
+            for x in tmp:
+                if x:
+                    pre += 1
+                else:
+                    ans = ans if ans > pre else pre
+                    pre = 0
+            ans = ans if ans > pre else pre
+            return ans
+
+        assert [ls[-1] for ls in tree.get()] == nums
+        assert tree.range_query(0, high - 1)[3] == check(nums)
+        assert tree.cover[1][3] == check(nums)
+        for _ in range(100):
+            op = random.randint(0, 3)
+            left = random.randint(0, high - 1)
+            right = random.randint(left, high - 1)
+            if op == 0:
+                tree.range_update(left, right, op)
+                for i in range(left, right + 1):
+                    nums[i] = 0
+            elif op == 1:
+                tree.range_update(left, right, op)
+                for i in range(left, right + 1):
+                    nums[i] = 1
+            elif op == 2:
+                tree.range_update(left, right, op)
+                for i in range(left, right + 1):
+                    nums[i] = 1 - nums[i]
+            elif op == 3:
+                assert tree.range_query(left, right)[-1] == sum(nums[left:right + 1])
+            else:
+                assert tree.range_query(left, right)[3] == check(nums[left:right + 1])
+            assert [ls[-1] for ls in tree.get()] == nums
+
+        assert [int(ls[0] == 1) for ls in tree.get()] == nums
         return
 
 
--- src/data_structure/zkw_segment_tree/template.py
+++ src/data_structure/zkw_segment_tree/template.py
@@ -213,3 +213,112 @@ class LazySegmentTree:
             left >>= 1
             right >>= 1
         return self.combine(ans_left, ans_right)
+
+
+class LazySegmentTreeLength:
+    def __init__(self, n, combine_cover, cover_initial, merge_cover_tag, merge_tag_tag, tag_initial):
+        self.n = n
+        self.cover_initial = cover_initial
+        self.merge_cover_tag = merge_cover_tag
+        self.merge_tag_tag = merge_tag_tag
+        self.tag_initial = tag_initial
+        self.lazy_tag = [self.tag_initial] * (2 * self.n)
+        self.h = 0
+        while (1 << self.h) < n:
+            self.h += 1
+        self.combine_cover = combine_cover
+        self.cover = [self.cover_initial] * (2 * self.n)
+        return
+
+    def build(self, nums):
+        for i in range(self.n):
+            if nums[i]:
+                self.cover[i + self.n] = (1, 1, 0, 1, 1, 1)
+            else:
+                self.cover[i + self.n] = (-1, -1, 1, 0, 1, 0)
+        for i in range(self.n - 1, 0, -1):
+            self.cover[i] = self.combine_cover(self.cover[i << 1], self.cover[(i << 1) | 1])
+        return
+
+    def push_up(self, i):
+        while i > 1:
+            i >>= 1
+            self.cover[i] = self.combine_cover(self.cover[i << 1], self.cover[(i << 1) | 1])
+        return
+
+    def push_down(self, i):
+        for s in range(self.h, 0, -1):
+            x = i >> s
+            if self.lazy_tag[x] != self.tag_initial:
+                self.make_tag(x << 1, self.lazy_tag[x])
+                self.make_tag((x << 1) | 1, self.lazy_tag[x])
+                self.lazy_tag[x] = self.tag_initial
+        x = i
+        if self.lazy_tag[x] != self.tag_initial:
+            if (i << 1) < self.n * 2:
+                self.make_tag(x << 1, self.lazy_tag[x])
+            if ((i << 1) | 1) < self.n * 2:
+                self.make_tag((x << 1) | 1, self.lazy_tag[x])
+            self.lazy_tag[x] = self.tag_initial
+        return
+
+    def make_tag(self, i, val):
+        self.cover[i] = self.merge_cover_tag(self.cover[i], val)
+        self.lazy_tag[i] = self.merge_tag_tag(val, self.lazy_tag[i])
+        return
+
+    def range_update(self, left, right, val):
+        left += self.n
+        right += self.n + 1
+        ll = left
+        rr = right
+        self.push_down(ll)
+        self.push_down(rr - 1)
+        while left < right:
+            if left & 1:
+                self.make_tag(left, val)
+                left += 1
+            if right & 1:
+                right -= 1
+                self.make_tag(right, val)
+            left >>= 1
+            right >>= 1
+        self.push_down(ll)
+        self.push_down(rr - 1)
+
+        self.push_up(ll)
+        self.push_up(rr - 1)
+        return
+
+    def get(self):
+        for i in range(1, self.n):
+            if self.lazy_tag[i] != self.tag_initial:
+                self.make_tag(i << 1, self.lazy_tag[i])
+                self.make_tag((i << 1) | 1, self.lazy_tag[i])
+                self.lazy_tag[i] = self.tag_initial
+        return self.cover[self.n:]
+
+    def point_get(self, ind):
+        ans = 0
+        ind += self.n
+        while ind > 0:
+            ans += self.lazy_tag[ind]
+            ind //= 2
+        return ans
+
+    def range_query(self, left, right):
+        ans_left = ans_right = self.cover_initial
+        left += self.n
+        right += self.n + 1
+        self.push_down(left)
+        self.push_down(right - 1)
+        while left < right:
+            if left & 1:
+                ans_left = self.combine_cover(ans_left, self.cover[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                ans_right = self.combine_cover(self.cover[right], ans_right)
+            left >>= 1
+            right >>= 1
+        return self.combine_cover(ans_left, ans_right)
