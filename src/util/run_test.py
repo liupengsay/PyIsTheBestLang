327294d7e2be843e662eaee0595349cba0233ca9
liupengsay <liupengsay@foxmail.com>
2024-11-10 11:04:54 +0800

    add abc175 daily

--- src/util/run_test.py
+++ src/util/run_test.py
@@ -445,7 +445,7 @@ class TestGeneral(unittest.TestCase):
         return
 
     def test_abc_problem(self):
-        num = "175"
+        num = "379"
         # webbrowser.open(f"https://atcoder.jp/contests/abc{num}")
         webbrowser.open(f"https://atcoder.jp/contests/abc{num}/standings")
         # webbrowser.open(f"https://atcoder.jp/contests/abc{num}/results")
